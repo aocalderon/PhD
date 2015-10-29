b5583d6fac8e83c8a66dada3e19b6f68f599a802
aocalderon <andress.calderon@gmail.com>
2015-10-29 17:19:27 +0000

    Adding the tex files for the report...

--- Y1Q1/GPU/lab2/kernel.cu
+++ Y1Q1/GPU/lab2/kernel.cu
@@ -8,7 +8,7 @@
 
 #include <stdio.h>
 
-#define TILE_SIZE 32
+#define TILE_SIZE 16
 
 __global__ void mysgemm(int m, int n, int k, const float *A, const float *B, float *C) {
 
@@ -24,39 +24,46 @@ __global__ void mysgemm(int m, int n, int k, const float *A, const float *B, flo
      ********************************************************************/
 
     // INSERT KERNEL CODE HERE
+    // Declaring the variables in shared memory...
     __shared__ float A_s[TILE_SIZE][TILE_SIZE];
 	__shared__ float B_s[TILE_SIZE][TILE_SIZE];
 
-	int bx = blockIdx.x;
-	int dx = blockDim.x;
+	// Finding the coordinates for the current thread...
 	int tx = threadIdx.x;
-	int by = blockIdx.y;
-	int dy = blockDim.y;
 	int ty = threadIdx.y;
-
-	int col = bx * dx + tx;
-	int row = by * dy + ty;
+	int col = blockIdx.x * blockDim.x + tx;
+	int row = blockIdx.y * blockDim.y + ty;
 
 	float sum = 0.0f;
 
 	for(int i = 0; i < ((k - 1) / TILE_SIZE) + 1; ++i){
+		// Validation in the case the thread tries to write in share 
+		// memory of the dimensions of matrix A...
 		if(row < m && (i * TILE_SIZE + tx) < k){
 			A_s[ty][tx] = A[(row * k) + (i * TILE_SIZE + tx)];
 		} else {
+			// In that case, just write a 0 which will no affect 
+			// the computation...
 			A_s[ty][tx] = 0.0f;
 		}
+		// Similar validation for B...
 		if((i * TILE_SIZE + ty) < k && col < n){
 			B_s[ty][tx] = B[((i * TILE_SIZE + ty) * n) + col];
 		} else {
 			B_s[ty][tx] = 0.0f;
 		}
+		// Wait for all the threads to write in share memory
 		__syncthreads();
 
+		// Compute the multiplication on the tile...
 		for(int j = 0; j < TILE_SIZE; ++j){
 			sum += A_s[ty][j] * B_s[j][tx];
 		}
+		// Wait to finish before to go ahead with the next phase...
 		__syncthreads();
 	}
+	// Write the final result in C just if it is inside of the valid 
+	// dimensions... 
 	if(row < m && col < n){
 		C[row * n + col] = sum;
 	}
@@ -84,17 +91,12 @@ void basicSgemm(char transa, char transb, int m, int n, int k, float alpha, cons
 		printf("unsupported value of beta\n");
 		return;
     }
-
-    // Initialize thread block and kernel grid dimensions ---------------------
-
     const unsigned int BLOCK_SIZE = TILE_SIZE;
 
+    // Initialize thread block and kernel grid dimensions
     const dim3 dim_block(BLOCK_SIZE, BLOCK_SIZE, 1);
 	const dim3 dim_grid(((n - 1) / BLOCK_SIZE) + 1, ((m - 1) / BLOCK_SIZE) + 1, 1);
 
-    // Invoke CUDA kernel -----------------------------------------------------
-
-    // INSERT CODE HERE
     // Calling the kernel with the above-mentioned setting... 
 	mysgemm<<<dim_grid, dim_block>>>(m, n, k, A, B, C);
 }
