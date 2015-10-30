27b76c39900d05e10cf3fae64006587fec3f0dba
aocalderon <andress.calderon@gmail.com>
2015-10-30 14:57:00 +0000

    Preparing the report...

--- Y1Q1/GPU/lab2/test4.sh
+++ Y1Q1/GPU/lab2/test4.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-# declare STRING variable
 date
-N=10
-for i in `seq 1000 1000 20000`; do
+for i in `seq 50 50 1000`; do
 	./sgemm-tiled $i
 	./sgemm-tiled $i
 	./sgemm-tiled $i
 done
 STRING="Done!!!"
-#print variable on a screen
 echo $STRING
 date
