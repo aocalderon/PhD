b71c995ea0bfad8b3ffdbfcbc4ca60e4d4bce27e
Andres Oswaldo Calderon Romero <andress.calderon@gmail.com>
2015-11-07 11:11:18 -0800

    Adding Lab 3...

---
+++ Y1Q1/GPU/lab3/3.1-reduction/kernel.cu
@@ -0,0 +1,50 @@
+/******************************************************************************
+ *cr
+ *cr            (C) Copyright 2010 The Board of Trustees of the
+ *cr                        University of Illinois
+ *cr                         All Rights Reserved
+ *cr
+ ******************************************************************************/
+
+#define BLOCK_SIZE 512
+
+__global__ void reduction(float *out, float *in, unsigned size)
+{
+    /********************************************************************
+    Load a segment of the input vector into shared memory
+    Traverse the reduction tree
+    Write the computed sum to the output vector at the correct index
+    ********************************************************************/
+
+    // INSERT KERNEL CODE HERE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Y1Q1/GPU/lab3/3.2-prefix-scan/kernel.cu
@@ -0,0 +1,55 @@
+/******************************************************************************
+ *cr
+ *cr            (C) Copyright 2010 The Board of Trustees of the
+ *cr                        University of Illinois
+ *cr                         All Rights Reserved
+ *cr
+ ******************************************************************************/
+
+#define BLOCK_SIZE 512
+
+// Define your kernels in this file you may use more than one kernel if you
+// need to
+
+// INSERT KERNEL(S) HERE
+
+
+
+
+
+
+
+
+
+/******************************************************************************
+Setup and invoke your kernel(s) in this function. You may also allocate more
+GPU memory if you need to
+*******************************************************************************/
+void preScan(float *out, float *in, unsigned in_size)
+{
+    // INSERT CODE HERE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
