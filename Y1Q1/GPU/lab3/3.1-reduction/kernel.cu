378539c81cf4236188040fdd2b9c51cf73d06e5c
aocalderon <andress.calderon@gmail.com>
2015-11-11 13:34:40 -0100

    Fixing some details in format code...

--- Y1Q1/GPU/lab3/3.1-reduction/kernel.cu
+++ Y1Q1/GPU/lab3/3.1-reduction/kernel.cu
@@ -16,30 +16,34 @@ __global__ void reduction(float *out, float *in, unsigned size)
     Write the computed sum to the output vector at the correct index
     ********************************************************************/
 
-    // INSERT KERNEL CODE HERE
-	
+	// Declare an array for share memory...
 	__shared__ float partialSum[2 * BLOCK_SIZE];
+	
+	// Initialize some variables to access data...
 	unsigned int t = threadIdx.x;
 	unsigned int start = 2 * blockIdx.x * blockDim.x;
-	// unsigned int c = blockDim.x * blockIdx.x + t;	
 
+	// Validation to avoid load data outside of the input array...
 	if(start + t < size)
 		partialSum[t] = in[start + t];
 	else
 		partialSum[t] = 0.0f;
 	
+	// Same validation for the other position...
 	if(start + blockDim.x + t < size)
 		partialSum[blockDim.x + t] = in[start + blockDim.x + t];
 	else
 		partialSum[blockDim.x + t] = 0.0f;
 
+	// Iterate through share memory to compute the sum...
 	for (int stride = blockDim.x; stride > 0; stride /= 2){
-		__syncthreads();
+		__syncthreads();  // Synchronize the share memory load and each iteration...
 		if (t < stride)
 			partialSum[t] += partialSum[t + stride];
 	} 
+	// Do not forget to synchronize last iteration...
 	__syncthreads();
 
+	// Copy back the result...
 	out[blockIdx.x] = partialSum[0];
-	
 }
--- Y1Q1/GPU/lab3/3.2-prefix-scan/kernel.cu
+++ Y1Q1/GPU/lab3/3.2-prefix-scan/kernel.cu
@@ -11,11 +11,8 @@
 // Define your kernels in this file you may use more than one kernel if you
 // need to
 
-// INSERT KERNEL(S) HERE
-
 __global__ void scan(float *out, float *in, unsigned size){
 	__shared__ float section[2 * BLOCK_SIZE];
-	
 	int t = blockDim.x * blockIdx.x + threadIdx.x;
 
 	if(t < size)
@@ -33,13 +30,12 @@ __global__ void scan(float *out, float *in, unsigned size){
 	}
 	
 	for(int stride = BLOCK_SIZE / 2; stride > 0; stride /= 2){
-		__syncthreads();
 		int index = (threadIdx.x + 1) * stride * 2 - 1;
-		if(index + stride < 2 * BLOCK_SIZE){
+		if(index + stride < 2 * BLOCK_SIZE)
 			section[index + stride] += section[index];
-		}
+		__syncthreads();
 	}
-	__syncthreads();
+	//__syncthreads();
 	if(t < size) 
 		out[t] = section[threadIdx.x];
 }
@@ -54,19 +50,14 @@ __global__ void post(float *out, float *n, unsigned size){
 Setup and invoke your kernel(s) in this function. You may also allocate more
 GPU memory if you need to
 *******************************************************************************/
-void preScan(float *out, float *in, unsigned size)
-{
-    // INSERT CODE HERE
+void preScan(float *out, float *in, unsigned size){
 	dim3 dim_block(BLOCK_SIZE, 1, 1);
 	dim3 dim_grid(size/BLOCK_SIZE + 1, 1, 1);
 	scan<<<dim_grid, dim_block>>>(out, in, size);
 }
 
-void postScan(float *out, float *n, unsigned size)
-{
-    // INSERT CODE HERE
+void postScan(float *out, float *n, unsigned size){
 	dim3 dim_block(BLOCK_SIZE, 1, 1);
 	dim3 dim_grid(size/BLOCK_SIZE + 1, 1, 1);
 	post<<<dim_grid, dim_block>>>(out, n, size);
 }
-
