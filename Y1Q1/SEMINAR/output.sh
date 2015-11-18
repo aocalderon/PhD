b2ce73140a19e3c66c2f68fe9f0ea82263a4781a
aocalderon <andress.calderon@gmail.com>
2015-11-18 20:51:48 -0100

    Working in presentation for Seminar...

---
+++ Y1Q1/SEMINAR/output.sh
@@ -0,0 +1,21 @@
+and@and-dblab:~/Documents/Projects/C++/word2vec/trunk$ ./demo-word.sh 
+make: Nothing to be done for 'all'.
+--2015-11-12 18:18:10--  http://mattmahoney.net/dc/text8.zip
+Resolving mattmahoney.net (mattmahoney.net)... 98.139.135.129
+Connecting to mattmahoney.net (mattmahoney.net)|98.139.135.129|:80... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 31344016 (30M) [application/zip]
+Saving to: ‘text8.gz’
+
+text8.gz                                            100%[==================================================>]  29.89M  1.74MB/s   in 18s    
+
+2015-11-12 18:18:28 (1.70 MB/s) - ‘text8.gz’ saved [31344016/31344016]
+
+Starting training using file text8
+Vocab size: 71291
+Words in train file: 16718843
+Alpha: 0.000005  Progress: 100.10%  Words/thread/sec: 113.47k  
+real	10m15.450s
+user	36m52.552s
+sys	0m4.388s
+Enter word or sentence (EXIT to break): 
---
+++ Y1Q1/SEMINAR/output2.sh
@@ -0,0 +1,14 @@
+and@and-dblab:~/Documents/Projects/C++/word2vec/trunk$ ./demo-phrases.sh 
+make: Nothing to be done for 'all'.
+--2015-11-12 18:33:08--  http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2012.en.shuffled.gz
+Resolving www.statmt.org (www.statmt.org)... 129.215.197.100
+Connecting to www.statmt.org (www.statmt.org)|129.215.197.100|:80... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 786717767 (750M) [application/x-gzip]
+Saving to: ‘news.2012.en.shuffled.gz’
+
+news.2012.en.shuffled.gz                            100%[============================================>] 750.27M  4.25MB/s   in 3m 7s  
+
+2015-11-12 18:36:16 (4.02 MB/s) - ‘news.2012.en.shuffled.gz’ saved [786717767/786717767]
+
+...
