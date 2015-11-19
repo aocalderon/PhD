7b5ab5f2c29952d972ed84cf605e1ec85f7f8912
aocalderon <andress.calderon@gmail.com>
2015-11-19 13:35:32 -0100

    Finishing presentation...

--- Y1Q1/SEMINAR/demo-phrase-accuracy.sh
+++ Y1Q1/SEMINAR/demo-phrase-accuracy.sh
@@ -1,3 +1,4 @@
+## Same than before...
 make
 if [ ! -e news.2012.en.shuffled ]; then
   wget http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2012.en.shuffled.gz
@@ -8,4 +9,5 @@ time ./word2phrase -train news.2012.en.shuffled-norm0 -output news.2012.en.shuff
 time ./word2phrase -train news.2012.en.shuffled-norm0-phrase0 -output news.2012.en.shuffled-norm0-phrase1 -threshold 100 -debug 2
 tr A-Z a-z < news.2012.en.shuffled-norm0-phrase1 > news.2012.en.shuffled-norm1-phrase1
 time ./word2vec -train news.2012.en.shuffled-norm1-phrase1 -output vectors-phrase.bin -cbow 1 -size 200 -window 10 -negative 25 -hs 0 -sample 1e-5 -threads 20 -binary 1 -iter 15
+## Test accuracy...
 ./compute-accuracy vectors-phrase.bin < questions-phrases.txt
