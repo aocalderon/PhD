9d35764e5a18afde9d657a50363836589219810a
aocalderon <andress.calderon@gmail.com>
2016-01-23 16:36:58 -0100

    Working in the report, fixing example code and some figures...

--- Y1Q2/Compilers/Project/example.R
+++ Y1Q2/Compilers/Project/example.R
@@ -1,17 +1,22 @@
-#a quick classification example
+require(knnflex)
+require(caret)
+
 # a quick classification example
-x1 <- c(rnorm(5,mean=1),rnorm(5,mean=5))
-x2 <- c(rnorm(5,mean=5),rnorm(5,mean=1))
+n <- 200
+set.seed(123)
+x1 <- c(rnorm(n/2,mean=2.5),rnorm(n/2,mean=7.5))
+x2 <- c(rnorm(n/2,mean=7.5),rnorm(n/2,mean=2.5))
 x  <- cbind(x1,x2)
-y <- c(rep(1,5),rep(0,5))
-train <- sample(1:10,7)
+y <- c(rep(1,n/2),rep(0,n/2))
+train <- sample(1:n,n*0.75)
+test <- (1:n)[-train]
 # plot the training cases
-plot(x1[train],x2[train],col=y[train]+1,xlab="x1",ylab="x2")
+plot(x1[train],x2[train],col=y[train]+1,xlab="x1",ylab="x2"
+     ,xlim=c(-1,10),ylim=c(-1,10))
 # predict the other cases
-test <- (1:10)[-train]
 kdist <- knn.dist(x)
 preds <- knn.predict(train,test,y,kdist,k=3,agg.meth="majority")
 # add the predictions to the plot
 points(x1[test],x2[test],col=as.integer(preds)+1,pch="+")
 # display the confusion matrix
-table(y[test],preds)
+confusionMatrix(y[test],preds)
