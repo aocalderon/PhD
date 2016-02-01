b122ef0f7d378b4f29e580b5f6c9aaf51734c0b6
aocalderon <andress.calderon@gmail.com>
2016-02-01 12:15:09 -0100

    New version of the report...

--- Y1Q2/OperativeSystems/HW1/test2.1.R
+++ Y1Q2/OperativeSystems/HW1/test2.1.R
@@ -5,7 +5,7 @@ data$time <- seq(0,200,5)
 data$w1 <- data$w1 - data$w1[1]
 data$w2 <- data$w2 - data$w2[1]
 data <- data[2:nrow(data),]
-pdf('test2.pdf')
+#pdf('test2.pdf')
 plot(data$time,data$w2,type='l',xlab='Time (secs)',ylab='Lottery wins',col=2)
 points(data$time,data$w2,col=2,cex=0.6,pch=22,bg=2)
 lines(data$time,data$w1,col=4)
@@ -13,5 +13,5 @@ points(data$time,data$w1,col=4,cex=0.6,pch=23,bg=4)
 legend('topleft', legend=c("Ticket inflation", "Fixed-ticket allocation")
        ,inset=c(0.01,0.02)
        ,col=c(2,4),lty=c(1,1),pch=22:24,pt.bg=c(2,4), cex=0.9)
-dev.off()
+#dev.off()
 
