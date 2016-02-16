aebbe670cc3e2b6a614f6c41c96d82fc4d1022a1
aocalderon <andress.calderon@gmail.com>
2016-02-15 21:45:26 -0800

    Plotting new datasets...

--- Y1Q2/Compilers/Project/M2/simulation_zoo.R
+++ Y1Q2/Compilers/Project/M2/simulation_zoo.R
@@ -1,10 +1,12 @@
 require(knnflex)
 require(caret)
 
-n <- nrow(iris)
-x <- iris[,1:4]
+zoo <- read.csv('zoo.csv', header = F)
+n <- nrow(zoo)
+m <- ncol(zoo)
+x <- zoo[,2:(m-1)]
 x <- apply(x,2,normalize)
-y <- iris[,5]
+y <- zoo[,m]
 
 set.seed(456)
 train <- sample(1:n,n*0.7)
@@ -13,17 +15,15 @@ kdist <- knn.dist(x)
 
 k <- 1 
 for(k in seq(1,9,2)){
-  preds <- knn.predict(train,test,y,kdist,k=k)
-  cm <- confusionMatrix(y[test],preds)
-  accuracy_orig <- cm$overall['Accuracy']
+  preds <- knn.predict(train,test,y,kdist,k=k, agg.meth = "majority")
+  accuracy_orig <- getAccuracy(y[test],preds)
   as <- c()
   range <- seq(0.01,1,0.01)
   for(p in range){
     kdist_error <- injectError(kdist, p)
-    preds_error <- knn.predict(train,test,y,kdist_error,k=k)
-    cm_error <- confusionMatrix(y[test],preds_error)
-    accuracy_error <- cm_error$overall['Accuracy']
-    # print(paste(accuracy_orig,accuracy_error,accuracy_orig-accuracy_error))
+    preds_error <- knn.predict(train,test,y,kdist_error,k=k, agg.meth = "majority")
+    accuracy_error <- getAccuracy(y[test],preds_error)
+    print(paste(accuracy_orig,accuracy_error,accuracy_orig-accuracy_error))
     as <- c(as, accuracy_error)
   }
   
