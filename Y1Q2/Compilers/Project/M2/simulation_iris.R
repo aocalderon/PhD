b17b71d107dbe640360cdec1be168edae41229cd
aocalderon <andress.calderon@gmail.com>
2016-02-17 01:14:36 -0800

    Removing random seeds in ErrorInjector and plotting new graphs...

--- Y1Q2/Compilers/Project/M2/simulation_iris.R
+++ Y1Q2/Compilers/Project/M2/simulation_iris.R
@@ -1,5 +1,4 @@
 require(knnflex)
-require(caret)
 
 n <- nrow(iris)
 x <- iris[,1:4]
@@ -19,11 +18,13 @@ for(k in seq(1,9,2)){
   as <- c()
   range <- seq(0.01,1,0.01)
   for(p in range){
-    kdist_error <- injectError(kdist, p)
-    preds_error <- knn.predict(train,test,y,kdist_error,k=k)
-    cm_error <- confusionMatrix(y[test],preds_error)
-    accuracy_error <- cm_error$overall['Accuracy']
-    as <- c(as, accuracy_error)
+    accuracy_error <- c()
+    for(q in 1:100){
+      kdist_error <- injectError(kdist, p)
+      preds_error <- knn.predict(train,test,y,kdist_error,k=k, agg.meth = "majority")
+      accuracy_error <- c(accuracy_error, getAccuracy(y[test],preds_error))
+    }
+    as <- c(as, mean(accuracy_error))
   }
   
   data <- data.frame(p=range, Accuracy=as)
