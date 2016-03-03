d3b421a77e4ae4f5fc6cd5001372ac7719928c12
aocalderon <andress.calderon@gmail.com>
2016-03-03 02:42:35 -0800

    Printing report and review...

--- Y1Q2/DataMining/HW3/grid.R
+++ Y1Q2/DataMining/HW3/grid.R
@@ -1,12 +1,14 @@
+require(tiff)
+
 im = readTIFF('pools3b.tif')
 im = as.raster(im[,,1:3])
-pdf("figures/grid3.pdf", 7.83, 5.17)
+#pdf("figures/grid3.pdf", 7.83, 5.17)
 plot(im)
 for(i in seq(0,500,50)){
-  lines(c(i,i), c(0,350),lwd=1.5,lty=2,col=4)
+  lines(c(i,i), c(0,350),lwd=1.1,lty=2,col=4)
 }
 for(i in seq(0,350,50)){
-  lines(c(0,520), c(i,i),lwd=1.5,lty=2,col=4)
+  lines(c(0,520), c(i,i),lwd=1.1,lty=2,col=4)
 }
 #dev.off()
 
@@ -18,8 +20,8 @@ for(i in seq(0,500,50)){
 
 points = read.csv('valpools.csv', header = F)
 points = points[,1:2]
-points(points[,1],328-points[,2],cex=0.9,pch=21,bg=2)
-dev.off()
+points(points[,1],328-points[,2],cex=0.8,pch=21,bg=1)
+#dev.off()
 
 # im = readTIFF('pools3.tif')
 # im = as.raster(im[,,1:3])
