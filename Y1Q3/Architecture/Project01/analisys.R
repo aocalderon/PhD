754d2dafda73dfce780cb1ac5db6f95699b16699
aocalderon <andress.calderon@gmail.com>
2016-04-15 14:12:08 -0700

    Adding project 1 in Architecture...

---
+++ Y1Q3/Architecture/Project01/analisys.R
@@ -0,0 +1,64 @@
+start <- 219
+top <- 20
+xlab <- "Number of commit instructions"
+
+data <- readLines("results_C1.txt") 
+data <- data[start:length(data) - 1]
+n <- length(data)
+empty <- rep(0, n)
+results <- data.frame(metric = empty, C1 = empty)
+for(i in 1:n){
+  result <- strsplit(strsplit(data[i], "#")[[1]][1], "\\s+")[[1]]
+  results[i,1] <- result[1]
+  results[i,2] <- as.numeric(result[2])
+}
+data <- readLines("results_C2.txt") 
+data <- data[start:length(data) - 1]
+results$C2 <- empty
+for(i in 1:n){
+  result <- strsplit(strsplit(data[i], "#")[[1]][1], "\\s+")[[1]]
+  results[i,3] <- as.numeric(result[2])
+}
+data <- readLines("results_C4.txt") 
+data <- data[start:length(data) - 1]
+results$C4 <- empty
+for(i in 1:n){
+  result <- strsplit(strsplit(data[i], "#")[[1]][1], "\\s+")[[1]]
+  results[i,4] <- as.numeric(result[2])
+}
+data <- readLines("results_C8.txt") 
+data <- data[start:length(data) - 1]
+results$C8 <- empty
+results$desc <- empty
+for(i in 1:n){
+  result <- strsplit(strsplit(data[i], "#")[[1]][1], "\\s+")[[1]]
+  results[i,5] <- as.numeric(result[2])
+  results[i,6] <- strsplit(data[i], "#")[[1]][2]
+}
+
+plot(1:4,results[15,2:5],type='l',axes=F,ylab=results[15,1]
+     ,main=results[15,6]
+     ,xlab="Number of commit instructions")
+points(1:4,results[15,2:5],cex=0.7,pch=21,bg=1)
+box()
+axis(1,at=1:4,labels=c(1,2,4,8))
+axis(2)
+
+results$min <- apply(results[,2:5],1,min)
+results$max <- apply(results[,2:5],1,max)
+results$range <- results$max - results$min
+results$sd <- apply(results[,2:5],1,sd)
+results$index <- results$sd / results$range
+
+results <- results[with(results,order(-index)),]
+results <- results[1:top,1:6]
+
+for(i in 1:top){
+  plot(1:4,results[i,2:5],type='l',axes=F,ylab=results[i,1]
+       ,main=results[i,6]
+       ,xlab=xlab)
+  box()
+  axis(1,at=1:4,labels=c(1,2,4,8))
+  axis(2)
+  points(1:4,results[i,2:5],cex=0.7,pch=21,bg=1)
+}
