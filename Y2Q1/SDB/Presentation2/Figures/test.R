b704004f53b09143dc51eac58023347f61d79f9f
aocalderon <andress.calderon@gmail.com>
2016-10-21 18:30:19 -0700

    Working on new figures...

--- Y2Q1/SDB/Presentation2/Figures/test.R
+++ Y2Q1/SDB/Presentation2/Figures/test.R
@@ -1,4 +1,5 @@
 library("sqldf", lib.loc="~/R/x86_64-pc-linux-gnu-library/3.2")
+library("plotrix", lib.loc="~/R/x86_64-pc-linux-gnu-library/3.2")
 
 distance <- function(x1, y1, x2, y2){
   return(sqrt((x1-x2)^2+(y1-y2)^2))
@@ -19,17 +20,36 @@ calculateDisk <- function(x1, y1, x2, y2){
   return(NULL)
 }
 
-epsilon = 2
+epsilon = 10
 r2 = (epsilon / 2)^2
-n = 5
+n = 50
+size = 3
 
 x = runif(n, 0, n)
 y = runif(n, 0, n)
 
-#pdf("test.pdf")
 par(mar=c(0.1,0.1,0.1,0.1))
-plot(x, y, asp=1, axes = F, xlim = c(0 - epsilon, n + epsilon), ylim = c(0 - epsilon, n + epsilon), xlab = "", ylab = "", pch = 21, cex = 0.8, col = 1, bg = 1 )
+pointset=data.frame(x=x,y=y)
+data = sqldf("SELECT p1.x AS x1, p1.y AS y1, p2.x AS x2, p2.y AS y2 FROM pointset p1 CROSS JOIN pointset p2 ")
+plot(1, asp=1, axes = F, xlim = c(0 - epsilon, n + epsilon), ylim = c(0 - epsilon, n + epsilon), xlab = "", ylab = "", type='n')
+for(i in 1:nrow(data)){
+  x1 = data[i,1]
+  y1 = data[i,2]
+  x2 = data[i,3]
+  y2 = data[i,4]
+  d = distance(x1,y1,x2,y2)
+  if(d <= epsilon && d != 0){
+    centers = calculateDisk(x1,y1,x2,y2)
+    draw.circle(centers[1], centers[2], epsilon/2, nv = 1000, border = 2, col = NA, lty = 2, lwd = 0.5)
+    draw.circle(centers[3], centers[4], epsilon/2, nv = 1000, border = 2, col = NA, lty = 2, lwd = 0.5)    
+  }
+}
+points(x, y, pch = 21, cex = 0.5, col = 1, bg = 1)
 box()
+
+pdf("test.pdf",width=size,height=size)
+par(mar=c(0.1,0.1,0.1,0.1))
+plot(1, asp=1, axes = F, xlim = c(0 - epsilon, n + epsilon), ylim = c(0 - epsilon, n + epsilon), xlab = "", ylab = "", type='n')
 pointset=data.frame(x=x,y=y)
 data = sqldf("SELECT p1.x AS x1, p1.y AS y1, p2.x AS x2, p2.y AS y2 FROM pointset p1 CROSS JOIN pointset p2 ")
 
@@ -41,9 +61,10 @@ for(i in 1:nrow(data)){
   d = distance(x1,y1,x2,y2)
   if(d <= epsilon && d != 0){
     centers = calculateDisk(x1,y1,x2,y2)
-    draw.circle(centers[1], centers[2], epsilon/2, nv = 1000, border = NULL, col = NA, lty = 2, lwd = 0.5)
-    draw.circle(centers[3], centers[4], epsilon/2, nv = 1000, border = NULL, col = NA, lty = 2, lwd = 0.5)    
+    draw.circle(centers[1], centers[2], epsilon/2, nv = 1000, border = 2, col = NA, lty = 2, lwd = 0.5)
+    draw.circle(centers[3], centers[4], epsilon/2, nv = 1000, border = 2, col = NA, lty = 2, lwd = 0.5)    
   }
 }
-
-#dev.off()
+points(x, y, pch = 21, cex = 0.5, col = 1, bg = 1 )
+box()
+dev.off()
