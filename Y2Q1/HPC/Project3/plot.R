9194200ac24c5927f3c0f4c1e2cf3817124cc348
aocalderon <andress.calderon@gmail.com>
2016-10-27 15:21:18 -0700

    Plotting results...

--- Y2Q1/HPC/Project3/plot.R
+++ Y2Q1/HPC/Project3/plot.R
@@ -1,13 +1,29 @@
+library(data.table)
+library(ggplot2)
+
 data <- read.csv("S_10P10_P10-30_01.csv", header = F)
+data <- rbind(data, read.csv("S_10P10_P10-30_02.csv", header = F))
+data <- rbind(data, read.csv("S_10P10_P10-30_03.csv", header = F))
+data <- rbind(data, read.csv("S_10P10_P10-30_04.csv", header = F))
+data <- rbind(data, read.csv("S_10P10_P10-30_05.csv", header = F))
 data <- data[,c(1,3,5)]
-names(data) = c("Implementation","Cores","Time")
-data$Implementation <- as.character(data$Implementation)
-data$Implementation[data$Implementation == "S0"] <- "1.Without Optimization"
-data$Implementation[data$Implementation == "S1"] <- "2.Removing Evens"
-data$Implementation[data$Implementation == "S2"] <- "3.Removing Bcast"
-data$Implementation[data$Implementation == "S3"] <- "4.Reorder Looops"
-g = ggplot(data=data, aes(x=factor(Cores), y=Time, group=Implementation)) +
-  geom_line(aes(linetype=Implementation, color=Implementation)) +
-  geom_point(aes(shape=Implementation, color=Implementation)) +
-  labs(x="Number of processors", y="Time (sec)")
+names(data) = c("S","Cores","Time")
+data <- data.table(data)
+data <- unique(data[, Time:=mean(Time), by=list(S, Cores)])
+
+g = ggplot(data=data, aes(x=factor(Cores), y=Time, group=S, colour=S, shape=S)) +
+  geom_line(aes(linetype=S)) +
+  geom_point(size=2.5) +
+  labs(x="Number of processors", y="Time (sec)") +
+  scale_colour_discrete(name  ="Optimization",
+                        breaks=c("S0", "S1","S2","S3"),
+                        labels=c("Control", "Remove evens", "Remove Bcast", "Reorder loops")) +
+  scale_shape_discrete(name  ="Optimization",
+                       breaks=c("S0", "S1","S2","S3"),
+                       labels=c("Control", "Remove evens", "Remove Bcast", "Reorder loops")) +
+  scale_linetype_discrete(name  ="Optimization",
+                          breaks=c("S0", "S1","S2","S3"),
+                          labels=c("Control", "Remove evens", "Remove Bcast", "Reorder loops")) 
+pdf("plot.pdf", width = 10.5, height = 7.5)
 plot(g)
+dev.off()
