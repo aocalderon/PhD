bad737ba3acf649e5f7b6b3e30a55406e5c530fc
aocalderon <andress.calderon@gmail.com>
2016-10-28 00:30:19 -0700

    Plotting benchmarking for cache sizes...

--- Y2Q1/HPC/Project3/plot2.R
+++ Y2Q1/HPC/Project3/plot2.R
@@ -3,7 +3,7 @@ library(ggplot2)
 library(reshape)
 library(xtable)
 
-data <- read.csv("s.csv", header = F)
+data <- read.csv("r.csv", header = F)
 data <- data[,c(1,3,5)]
 names(data) = c("S","Cores","Time")
 data <- data.table(data)
@@ -21,7 +21,7 @@ g = ggplot(data=data, aes(x=factor(Cores), y=Time, group=S, colour=S, shape=S)) 
     scale_shape_discrete(name = legend_title,  breaks = breaks, labels = labels) +
     scale_linetype_discrete(name = legend_title, breaks = breaks, labels = labels)
 
-pdf("plot.pdf", width = 10.5, height = 7.5)
+pdf("plot2.pdf", width = 10.5, height = 7.5)
 plot(g)
 dev.off()
 
@@ -39,8 +39,8 @@ color2D.matplot(table[,2:5],
 axis(3, at = seq_len(ncol(table) - 1) - 0.5, labels = labels, tick = FALSE, cex.axis = 0.75)
 axis(2, at = seq_len(nrow(table)) - 0.5, labels = rev(table$Cores), tick = FALSE, las = 1, cex.axis = 0.75)
 
-table = xtable(table, caption = "Comparing the four versions of the Sieve of Erastosthenes.", label = "tab:table", align="cccccc")
+table = xtable(table, caption = "Comparing the four versions of the Sieve of Erastosthenes.", label = "tab:table2", align="cccccc")
 addtorow <- list()
 addtorow$pos <- list(0)
 addtorow$command <- c(paste0("\\textbf{Number of} & \\multicolumn{4}{c}{\\textbf{", legend_title,"}} \\\\\n \\textbf{Cores} & \\textbf{", paste0(labels, collapse = "} & \\textbf{"), "} \\\\\n"))
-print(table, include.rownames=F, add.to.row = addtorow, booktabs = T, include.colnames=F, file="tbl_table.tex")
+print(table, include.rownames=F, add.to.row = addtorow, booktabs = T, include.colnames=F, file="tbl_table2.tex")
---
+++ Y2Q1/HPC/Project3/plotCacheAnalysis.R
@@ -0,0 +1,49 @@
+library(data.table)
+library(ggplot2)
+library(reshape)
+library(xtable)
+
+data <- read.csv("t.csv", header = F)
+data <- data[,c(1,2,6)]
+names(data) = c("CO","CP","Time")
+data <- data.table(data)
+data <- unique(data[, Time:=mean(Time), by=list(CO, CP)])
+
+# legend_title = "Optimization"
+# breaks = c("S0", "S1","S2","S3")
+# labels = c("Baseline", "Remove evens", "Remove Bcast", "Reorder loops")
+# 
+# g = ggplot(data=data, aes(x=factor(Cores), y=Time, group=S, colour=S, shape=S)) +
+#   geom_line(aes(linetype=S)) +
+#   geom_point(size=2.5) +
+#   labs(x="Number of processors", y="Time (sec)") +
+#   scale_colour_discrete(name = legend_title, breaks = breaks, labels = labels) +
+#   scale_shape_discrete(name = legend_title,  breaks = breaks, labels = labels) +
+#   scale_linetype_discrete(name = legend_title, breaks = breaks, labels = labels)
+# 
+# pdf("plot2.pdf", width = 10.5, height = 7.5)
+# plot(g)
+# dev.off()
+
+table = cast(data, CO ~ CP)
+# names(table) = c("Cache", labels)
+table[4,6] = NA
+pdf("cache.pdf", width = 7, height = 5)
+color2D.matplot(table[,2:ncol(table)], 
+                show.values = 6,
+                axes = F,
+                xlab = "Cache size for the prime array",
+                ylab = "Cache size for the odds array",
+                vcex = 0.75,
+                vcol = "#555555",
+                na.color = "darkgreen",
+                extremes = c("green","yellow", "red"))
+axis(1, at = seq_len(ncol(table) - 1) - 0.5, labels = names(table)[2:ncol(table)], tick = FALSE, cex.axis = 0.75)
+axis(2, at = seq_len(nrow(table)) - 0.5, labels = rev(table$CO), tick = FALSE, las = 1, cex.axis = 0.75)
+textbox(c(4.15,5), 2.6, c("0.370178"), box = F, cex=.85, col="black")
+dev.off()
+# table = xtable(table, caption = "Comparing the different values of cache size for odds and prime arrays.", label = "tab:table3", align="cccccc")
+# addtorow <- list()
+# addtorow$pos <- list(0)
+# addtorow$command <- c(paste0("\\textbf{Number of} & \\multicolumn{4}{c}{\\textbf{", legend_title,"}} \\\\\n \\textbf{Cores} & \\textbf{", paste0(labels, collapse = "} & \\textbf{"), "} \\\\\n"))
+# print(table, include.rownames=F, add.to.row = addtorow, booktabs = T, include.colnames=F, file="tbl_table3.tex")
