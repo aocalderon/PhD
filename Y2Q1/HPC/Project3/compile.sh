cc3ff0e0c5d32b66261cec3f8dfdcafe07633fa9
aocalderon <andress.calderon@gmail.com>
2016-11-07 21:49:56 -0800

    Fixing some minor changes...

---
+++ Y2Q1/HPC/Project3/compile.sh
@@ -0,0 +1,5 @@
+[acald013@head ~]$ mpicc -O3 -o sieve0 sieve0.c -lm
+[acald013@head ~]$ mpicc -O3 -o sieve1 sieve1.c -lm
+[acald013@head ~]$ mpicc -O3 -o sieve2 sieve2.c -lm
+[acald013@head ~]$ mpicc -O3 -o sieve3 sieve3.c -lm
+
