6b746566bd6b9eaf9cf3d5a72c2189d505b425f5
aocalderon <andress.calderon@gmail.com>
2016-11-25 07:44:47 -0800

    Running some experiments...

--- Y2Q1/SDB/Project/Code/Scripts/runExperimentsPorto.sh
+++ Y2Q1/SDB/Project/Code/Scripts/runExperimentsPorto.sh
@@ -2,9 +2,9 @@
 
 START=1
 STEP=1
-END=1
+END=10
 
-for dataset in 16M
+for dataset in 1M 2M 4M 
 do
 	cd /home/and/Documents/PhD/Code/Y2Q1/SDB/Project/Code/Scripts/pbfe2
 	./pbfe.job $START $STEP $END /opt/Datasets/Porto/P${dataset}.csv
--- Y2Q1/SDB/Project/Code/Scripts/runExperimentsPorto2.sh
+++ Y2Q1/SDB/Project/Code/Scripts/runExperimentsPorto2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OUTPUT="PortoTest"
+OUTPUT="Porto1M-4M_E1-10"
 
 ./runExperimentsPorto.sh > ${OUTPUT}.csv
-echo "Porto 16M, Epsilon 1." | mail -s "Porto experiments..." -A ${OUTPUT}.csv acald013@ucr.edu
+echo "Porto 1M 2M 4M, Epsilon 1 to 10." | mail -s "Porto experiments..." -A ${OUTPUT}.csv acald013@ucr.edu
