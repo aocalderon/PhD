cc8df5cc9e2523d2deddbabbebeddde09cf8e312
aocalderon <andress.calderon@gmail.com>
2016-11-24 19:53:53 -0800

    Adding animations and plots to visualize the results...

--- Y2Q1/SDB/Project/Results/Beijing10K-100K_E10-200.R
+++ Y2Q1/SDB/Project/Results/Beijing10K-100K_E10-200.R
@@ -3,10 +3,10 @@ library(ggplot2)
 library(reshape)
 library(xtable)
 
-data <- read.csv("r001.csv", header = F)
+data <- read.csv("Beijing10K-100K_E10-200.csv", header = F)
 dataset <- "50K"
-
 #data <- data[data$V3 == dataset,c(1,2,5)]
+
 data <- data[,c(1,2,3,5)]
 names(data) = c("Algorithm","Epsilon", "Dataset", "Time")
 reorder = c("10K", "20K", "30K", "40K", "50K", "60K", "70K", "80K", "90K", "100K")
@@ -14,32 +14,21 @@ data$Dataset = factor(data$Dataset, levels = reorder)
 data <- data.table(data)
 # data <- unique(data[, Time:=mean(Time), by=list(Algorithm, Epsilon)])
 
-#legend_title = "Algorithm"
-#breaks = c("S0", "S1","S2","S3")
-#labels = c("Baseline", "Remove evens", "Remove Bcast", "Reorder loops")
-
+title=expression(paste("Execution time by ",epsilon," (radius of disk in mts) in Beijing dataset."))
 g = ggplot(data=data, aes(x=factor(Dataset), y=Time, group=Algorithm, colour=Algorithm, shape=Algorithm)) +
   geom_line(aes(linetype=Algorithm)) +
   geom_point(size=2.5) +
-  labs(x="Epsilon (mts)", y="Time (sec)") + facet_wrap(~Epsilon)
+  labs(title=title,y="Time (sec)") + 
+  scale_x_discrete("Number of points", breaks=c("20K","40K","60K","80K", "100K")) +
+  theme(axis.text.x=element_text(size=8, angle=90),axis.text.y=element_text(size=8)) +
+  facet_wrap(~Epsilon)
 
-#pdf("plot.pdf", width = 10.5, height = 7.5)
+pdf("Beijing10K-100K_E10-200.pdf", width = 10.5, height = 7.5)
 plot(g)
-#dev.off()
+dev.off()
 
 # table = cast(data, Cores ~ S)
 # names(table) = c("Cores", labels)
-# 
-# color2D.matplot(table[,2:5], 
-#                 show.values = T,
-#                 axes = F,
-#                 xlab = "",
-#                 ylab = "",
-#                 vcex = 0.75,
-#                 vcol = "black",
-#                 extremes = c("green","yellow", "red"))
-# axis(3, at = seq_len(ncol(table) - 1) - 0.5, labels = labels, tick = FALSE, cex.axis = 0.75)
-# axis(2, at = seq_len(nrow(table)) - 0.5, labels = rev(table$Cores), tick = FALSE, las = 1, cex.axis = 0.75)
 # 
 # table = xtable(table, caption = "Comparing the four versions of the Sieve of Erastosthenes.", label = "tab:table1", align="cccccc")
 # addtorow <- list()
