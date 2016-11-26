8e822be7f020692c325b40cb079a905535726859
aocalderon <andress.calderon@gmail.com>
2016-11-26 07:47:28 -0800

    Running Porto_N0.5M-1M_E2-7...

--- Y2Q1/SDB/Project/Code/Scripts/runExperimentsPorto.sh
+++ Y2Q1/SDB/Project/Code/Scripts/runExperimentsPorto.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-START=1
+START=2
 STEP=1
-END=10
+END=7
 
-for dataset in 1M 2M 4M 
+for dataset in 1_5M 2M 2_5M 3M 3_5M 4M 
 do
 	cd /home/and/Documents/PhD/Code/Y2Q1/SDB/Project/Code/Scripts/pbfe2
 	./pbfe.job $START $STEP $END /opt/Datasets/Porto/P${dataset}.csv
--- Y2Q1/SDB/Project/Code/Scripts/runExperimentsPorto2.sh
+++ Y2Q1/SDB/Project/Code/Scripts/runExperimentsPorto2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OUTPUT="Porto1M-4M_E1-10"
+OUTPUT="Porto_N1.5M-4M_E2-7"
 
 ./runExperimentsPorto.sh > ${OUTPUT}.csv
-echo "Porto 1M 2M 4M, Epsilon 1 to 10." | mail -s "Porto experiments..." -A ${OUTPUT}.csv acald013@ucr.edu
+echo "Porto 1.5M to 4M, Epsilon 2 to 7." | mail -s "Porto experiments..." -A ${OUTPUT}.csv acald013@ucr.edu
