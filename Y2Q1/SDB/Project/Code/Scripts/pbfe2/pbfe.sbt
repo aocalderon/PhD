679f0b724df3e027daebe67a3e5e63fc90489bc4
aocalderon <andress.calderon@gmail.com>
2016-11-28 07:23:16 -0800

    Running another experiment...

---
+++ Y2Q1/SDB/Project/Code/Scripts/pbfe2/pbfe.sbt
@@ -0,0 +1,5 @@
+name := "PBFE"
+version := "1.0"
+scalaVersion := "2.10.6"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.6.0"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "1.6.0"
---
+++ Y2Q1/SDB/Project/Code/Scripts/runExperimentsBeijing.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+START=10
+STEP=10
+END=200
+
+for dataset in 10K 20K 30K 40K 50K 60K 70K 80K 90K 100K  
+do
+	cd /home/and/Documents/PhD/Code/Y2Q1/SDB/Project/Code/Scripts/pbfe2
+	./pbfe.job $START $STEP $END /opt/Datasets/Beijing/P${dataset}.csv
+	#cd /home/and/Documents/PhD/Code/Y2Q1/SDB/Project/Code/Scripts/pbfe3
+	#./pbfe.job $START $STEP $END /opt/Datasets/Beijing/P${dataset}.csv
+done
