3181ce27a63e8cbede409a9c5a34054f30d09490
aocalderon <andress.calderon@gmail.com>
2016-12-01 09:27:24 -0800

    Plotting some results...

--- Y2Q1/SDB/Project/Results/Beijing10K-100K_E10-200/Beijing10K-100K_E10-200.R
+++ Y2Q1/SDB/Project/Results/Beijing10K-100K_E10-200/Beijing10K-100K_E10-200.R
@@ -1,23 +1,15 @@
-library(data.table)
 library(ggplot2)
-library(reshape)
-library(xtable)
 
 data <- read.csv("Beijing10K-100K_E10-200.csv", header = F)
-dataset <- "50K"
-#data <- data[data$V3 == dataset,c(1,2,5)]
-
 data <- data[,c(1,2,3,5)]
 names(data) = c("Algorithm","Epsilon", "Dataset", "Time")
 reorder = c("10K", "20K", "30K", "40K", "50K", "60K", "70K", "80K", "90K", "100K")
 data$Dataset = factor(data$Dataset, levels = reorder)
-data <- data.table(data)
-# data <- unique(data[, Time:=mean(Time), by=list(Algorithm, Epsilon)])
 
 title=expression(paste("Execution time by ",epsilon," (radius of disk in mts) in Beijing dataset."))
 g = ggplot(data=data, aes(x=factor(Dataset), y=Time, group=Algorithm, colour=Algorithm, shape=Algorithm)) +
   geom_line(aes(linetype=Algorithm)) +
-  geom_point(size=2.5) +
+  geom_point(size=2) +
   labs(title=title,y="Time (sec)") + 
   scale_x_discrete("Number of points", breaks=c("20K","40K","60K","80K", "100K")) +
   theme(axis.text.x=element_text(size=8, angle=90),axis.text.y=element_text(size=8)) +
@@ -26,12 +18,3 @@ g = ggplot(data=data, aes(x=factor(Dataset), y=Time, group=Algorithm, colour=Alg
 pdf("Beijing10K-100K_E10-200.pdf", width = 10.5, height = 7.5)
 plot(g)
 dev.off()
-
-# table = cast(data, Cores ~ S)
-# names(table) = c("Cores", labels)
-# 
-# table = xtable(table, caption = "Comparing the four versions of the Sieve of Erastosthenes.", label = "tab:table1", align="cccccc")
-# addtorow <- list()
-# addtorow$pos <- list(0)
-# addtorow$command <- c(paste0("\\textbf{Number of} & \\multicolumn{4}{c}{\\textbf{", legend_title,"}} \\\\\n \\textbf{Cores} & \\textbf{", paste0(labels, collapse = "} & \\textbf{"), "} \\\\\n"))
-# print(table, include.rownames=F, add.to.row = addtorow, booktabs = T, include.colnames=F, file="tbl_table.tex")
---
+++ Y2Q1/SDB/Project/Results/Beijing_PBFE2vsPBFE3vsPBFE4_N10K-100K_E10-200/Beijing_PBFE2vsPBFE3vsPBFE4_N10K-100K_E10-200.R
@@ -0,0 +1,21 @@
+library(ggplot2)
+
+data <- read.csv("Beijing_PBFE2vsPBFE3vsPBFE4_N10K-100K_E10-200.csv", header = F)
+
+data <- data[,c(1,2,3,5)]
+names(data) = c("Algorithm","Epsilon", "Dataset", "Time")
+reorder = paste0(seq(10,100,10),"K")
+data$Dataset = factor(data$Dataset, levels = reorder)
+
+title=expression(paste("Execution time by ",epsilon," (radius of disk in mts) in Beijing dataset."))
+g = ggplot(data=data, aes(x=factor(Dataset), y=Time, group=Algorithm, colour=Algorithm, shape=Algorithm)) +
+  geom_line(aes(linetype=Algorithm)) +
+  geom_point(size=2) +
+  labs(title=title,y="Time (sec)") + 
+  scale_x_discrete("Number of points", breaks=c("20K","40K","60K","80K", "100K")) +
+  theme(axis.text.x=element_text(size=8, angle=90),axis.text.y=element_text(size=8)) +
+  facet_wrap(~Epsilon)
+
+pdf("Beijing_PBFE2vsPBFE3vsPBFE4_N10K-100K_E10-200.pdf", width = 10.5, height = 7.5)
+plot(g)
+dev.off()
