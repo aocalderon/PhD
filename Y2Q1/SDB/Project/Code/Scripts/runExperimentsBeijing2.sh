e8e4df4ad684a00d14c63a43c4a193aca9c69e45
aocalderon <andress.calderon@gmail.com>
2016-12-02 11:45:18 -0800

    Adding more experiments...

--- Y2Q1/SDB/Project/Code/Scripts/runExperimentsBeijing2.sh
+++ Y2Q1/SDB/Project/Code/Scripts/runExperimentsBeijing2.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-OUTPUT="Beijing_PBFE2vsPBFE3vsPBFE4_N10K-100K_E10-200"
+OUTPUT="Beijing_PBFE_C1_N10K-100K_E10-200"
 START=`date`
-{ time ./runExperimentsBeijing.sh > ${OUTPUT}.csv; } 2> .time
+{ time ./runExperimentsBeijing.sh local > ${OUTPUT}.csv; } 2> .time
 TIME=`tail -n 3 .time`
 END=`date`
 echo -e "${TIME}\nDone!!!"
 
-MSG="Comparing PBFE DATAFRAME to RDD vs PBFE DATAFRAME vs PBFE SQL."
+MSG="Running PBFE with just one core."
+
 echo -e "START:\n${START}\nDATASET:\n${OUTPUT}\nEND:\n${END}\nTIME:\n${TIME}\n\n${MSG}" | mail -s "${OUTPUT} ${START}..." -A ${OUTPUT}.csv acald013@ucr.edu
 echo "Email sent."
