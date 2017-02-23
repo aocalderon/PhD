86ace7718ce0d90fd4048296436dda4fe23ca2eb
aocalderon <andress.calderon@gmail.com>
2017-02-22 23:53:07 -0800

    Fixing scripts to generate pruning figures...

--- Y2Q2/Research/notebooks/captureMap.sh
+++ Y2Q2/Research/notebooks/captureMap.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cutycapt --min-width=1680 --min-height=1050 --delay=2000 --url=file:///home/and/Documents/PhD/Code/Y2Q2/Research/notebooks/$1.html --out=$1.png
+
+xvfb-run --server-args="-screen 0, 1680x1050x24" cutycapt --min-width=1680 --min-height=1050 --delay=5000 --url=file:///home/and/Documents/PhD/Code/Y2Q2/Research/notebooks/${1}.html --out=${1}.png
