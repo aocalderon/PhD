66fbe3af4763bcc0b4eba5231490f3c50cbe69f4
aocalderon <andress.calderon@gmail.com>
2017-03-14 10:03:41 -0700

    Updating pbfe sources...

--- Y2Q1/SDB/Project/Code/Scripts/runExperimentsBeijing.sh
+++ Y2Q1/SDB/Project/Code/Scripts/runExperimentsBeijing.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-MASTER=$1
-LOG=$2
 START=10
 STEP=10
 END=200
@@ -10,9 +8,7 @@ for dataset in 10K 20K 30K 40K 50K 60K 70K 80K 90K 100K
 do
 	for epsilon in `seq $START $STEP $END`
 	do
-		spark-submit --master $MASTER pbfe.jar /opt/Datasets/Beijing/P${dataset}.csv $epsilon $LOG
+		spark-submit --master local[4] pbfe.jar /opt/Datasets/Beijing/P${dataset}.csv $epsilon
 	done
 done
 
-#cd /home/and/Documents/PhD/Code/Y2Q1/SDB/Project/Code/Scripts/pbfe4
-#./pbfe.job $START $STEP $END /opt/Datasets/Beijing/P${dataset}.csv
