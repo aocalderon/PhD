25263d54d17c8985ab43a06074c3c5fd341497b9
aocalderon <andress.calderon@gmail.com>
2017-03-17 21:25:12 -0700

    Working on PBFE-SPMF integration...

--- Y2Q2/Research/Code/pbfe2/src/main/scala/TestMain.scala
+++ Y2Q2/Research/Code/pbfe2/src/main/scala/TestMain.scala
@@ -15,13 +15,14 @@
  */
 
 import java.net.URL
+import java.util
 
 import edu.utah.cs.simba.SimbaContext
 import edu.utah.cs.simba.index.RTreeType
 import org.apache.spark.{SparkConf, SparkContext}
 
 import scala.collection.mutable.ListBuffer
-import ca.pfv.spmf.algorithms.frequentpatterns.lcm.{Dataset, AlgoLCM}
+import ca.pfv.spmf.algorithms.frequentpatterns.lcm.{AlgoLCM, Dataset}
 import ca.pfv.spmf.patterns.itemset_array_integers_with_count.Itemsets
 
 /**
@@ -60,15 +61,85 @@ object TestMain {
     println(df.queryExecution)
     df.show()
 
-    val filename = "/opt/Java/IdeaProjects/SPMF/src/ca/pfv/spmf/algorithms/frequentpatterns/lcm/contextPasquier99.txt"
     val minsup = 0.4
-    val input = java.net.URLDecoder.decode(filename,"UTF-8")
-    val dataset = new Dataset(input)
-    val algo = new AlgoLCM()
-    val itemsets = algo.runAlgorithm(minsup, dataset, null)
-
-    algo.printStats();
-    itemsets.printItemsets(dataset.getTransactions().size());
+    val t1 = new util.ArrayList[Integer]()
+    t1.add(1)
+    t1.add(Integer.valueOf(2))
+    t1.add(Integer.valueOf(5))
+    t1.add(Integer.valueOf(7))
+    t1.add(Integer.valueOf(9))
+    val t2 = new util.ArrayList[Integer]()
+    t2.add(Integer.valueOf(1))
+    t2.add(Integer.valueOf(3))
+    t2.add(Integer.valueOf(5))
+    t2.add(Integer.valueOf(7))
+    t2.add(Integer.valueOf(9))
+    val t3 = new util.ArrayList[Integer]()
+    t3.add(Integer.valueOf(1))
+    t3.add(Integer.valueOf(4))
+    t3.add(Integer.valueOf(1))
+    t3.add(Integer.valueOf(3))
+    val t4 = new util.ArrayList[Integer]()
+    t4.add(Integer.valueOf(1))
+    t4.add(Integer.valueOf(3))
+    t4.add(Integer.valueOf(4))
+    t4.add(Integer.valueOf(5))
+    t4.add(Integer.valueOf(6))
+    val t5 = new util.ArrayList[Integer]()
+    t5.add(Integer.valueOf(1))
+    t5.add(Integer.valueOf(2))
+    val t6 = new util.ArrayList[Integer]()
+    t6.add(Integer.valueOf(2))
+    t6.add(Integer.valueOf(1))
+    val t7 = new util.ArrayList[Integer]()
+    t7.add(Integer.valueOf(1))
+    t7.add(Integer.valueOf(7))
+    t7.add(Integer.valueOf(2))
+    t7.add(Integer.valueOf(3))
+    t7.add(Integer.valueOf(4))
+    t7.add(Integer.valueOf(5))
+    t7.add(Integer.valueOf(8))
+    t7.add(Integer.valueOf(9))
+    val t8 = new util.ArrayList[Integer]()
+    t8.add(Integer.valueOf(6))
+    t8.add(Integer.valueOf(1))
+    t8.add(Integer.valueOf(2))
+    val t9 = new util.ArrayList[Integer]()
+    t9.add(Integer.valueOf(4))
+    t9.add(Integer.valueOf(5))
+    t9.add(Integer.valueOf(6))
+    val t10 = new util.ArrayList[Integer]()
+    t10.add(Integer.valueOf(8))
+    t10.add(Integer.valueOf(2))
+    t10.add(Integer.valueOf(5))
+    val t11 = new util.ArrayList[Integer]()
+    t11.add(Integer.valueOf(9))
+    t11.add(Integer.valueOf(2))
+    t11.add(Integer.valueOf(1))
+    val t12 = new util.ArrayList[Integer]()
+    t12.add(Integer.valueOf(1))
+    t12.add(Integer.valueOf(2))
+    t12.add(Integer.valueOf(4))
+    t12.add(Integer.valueOf(8))
+    t12.add(Integer.valueOf(9))
+    val ts = new util.ArrayList[util.ArrayList[Integer]]
+    ts.add(t1)
+    ts.add(t2)
+    ts.add(t3)
+    ts.add(t4)
+    ts.add(t5)
+    ts.add(t6)
+    ts.add(t7)
+    ts.add(t8)
+    ts.add(t9)
+    ts.add(t10)
+    ts.add(t11)
+    ts.add(t12)
+    val dataset = new Dataset(ts)
+    val algo = new AlgoLCM
+    val itemsets = algo.runAlgorithm(minsup, dataset, null.asInstanceOf[String])
+    algo.printStats()
+    itemsets.printItemsets(dataset.getTransactions.size)
 
     //    leftDF.range(Array("x", "y"), Array(4.0, 5.0), Array(111.0, 222.0)).show(100)
     //    leftDF.knnJoin(rightDF, Array("x", "y"), Array("x", "y"), 3).show(100)
