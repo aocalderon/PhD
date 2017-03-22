82afa238aa5fe523533090e75bd5479f9c7cc340
aocalderon <andress.calderon@gmail.com>
2017-03-21 21:44:05 -0700

    Fixing a mess...

--- Y2Q2/Research/Code/runExperimentsBeijing.sh
+++ Y2Q2/Research/Code/runExperimentsBeijing.sh
@@ -2,13 +2,13 @@
 
 START=10
 STEP=10
-END=10
+END=50
 
-for dataset in 10K #20K 30K 40K 50K 60K 70K 80K 90K 100K  
+for dataset in 10K 20K 30K 40K 50K #60K 70K 80K 90K 100K  
 do
 	for epsilon in `seq $START $STEP $END`
 	do
-		spark-submit --packages com.databricks:spark-csv_2.10:1.5.0 --jars pbfe2/lib/SPMF.jar --master local[*] pbfe.jar /opt/Datasets/Beijing/P${dataset}.csv $epsilon
+		spark-submit --packages com.databricks:spark-csv_2.10:1.5.0 --master local[*] pbfe.jar /opt/Datasets/Beijing/P${dataset}.csv $epsilon
 		cat tdisks/part-* > datasets/Beijing_N${dataset}_E${epsilon}.centers
 		rm -fR tdisks/
 	done
