967fcbac0d3a343298c1e19655d402720d1a4d63
aocalderon <andress.calderon@gmail.com>
2017-03-23 10:33:12 -0700

    Finally compiling PBFE3.jar...

--- Y2Q2/Research/Code/PBFE3/src/main/scala/PBFE3.scala
+++ Y2Q2/Research/Code/PBFE3/src/main/scala/PBFE3.scala
@@ -1,7 +1,6 @@
 package main.scala
 
-import java.util
-import java.util.{Collections, Comparator}
+import java.util.{Collections, Calendar, ArrayList}
 
 import ca.pfv.spmf.algorithms.frequentpatterns.fpgrowth.AlgoFPMax
 import edu.utah.cs.simba.SimbaContext
@@ -14,12 +13,14 @@ import org.apache.spark.sql.Row
   */
 
 object PBFE3 {
+
   case class PointItem(id: Int, x: Double, y: Double)
+
   case class Pair(id1: Int, id2: Int, x1: Double, y1: Double, x2: Double, y2: Double)
 
   var epsilon: Double = 100.0
-  var filename: String = "/opt/Datasets/Beijing/B5.csv"
-  var r2: Double = math.pow(epsilon/2,2)
+  var filename: String = "/opt/Datasets/Beijing/B89.csv"
+  var r2: Double = math.pow(epsilon / 2, 2)
   var X = 0.0
   var Y = 0.0
   var D2 = 0.0
@@ -29,7 +30,7 @@ object PBFE3 {
   var k1 = 0.0
   var k2 = 0.0
 
-  def calculateDisks(pair: Row) : Pair = {
+  def calculateDisks(pair: Row): Pair = {
     X = pair.getDouble(1) - pair.getDouble(4)
     Y = pair.getDouble(2) - pair.getDouble(5)
     D2 = math.pow(X, 2) + math.pow(Y, 2)
@@ -62,48 +63,62 @@ object PBFE3 {
 
     var time1 = System.currentTimeMillis()
     val pairs = p1.distanceJoin(p2, Array("x", "y"), Array("x2", "y2"), epsilon)
-    val disks = pairs.rdd.filter( (x:Row) => x.getInt(0) > x.getInt(3) ).map( (x: Row) => calculateDisks(x) )
+    val disks = pairs.rdd.filter((x: Row) => x.getInt(0) > x.getInt(3)).map((x: Row) => calculateDisks(x))
     val ndisks = disks.count()
-    val time2 = System.currentTimeMillis()
+    var time2 = System.currentTimeMillis()
+    val diskGenerationTime = (time2 - time1) / 1000.0
 
-    println("PBFE3," + epsilon + "," + tag + "," + 2*ndisks + "," + (time2 - time1) / 1000.0 + "," + util.Calendar.getInstance().getTime)
+    val centers1 = disks.toDF.select("x1", "y1")
+    val centers2 = disks.toDF.select("x2", "y2")
+    val centers = centers1.unionAll(centers2)
 
-        time1 = System.currentTimeMillis()
-        val centers1 = disks.toDF.select("x1", "y1")
-        val centers2 = disks.toDF.select("x2", "y2")
-        val centers = centers1.unionAll(centers2)
+    val members = centers.distanceJoin(p1, Array("x1", "y1"), Array("x", "y"), (epsilon / 2) + 0.01)
+      .select("x1", "y1", "id")
+      .rdd
+      .map { d => (d(0) + "-" + d(1), d(2)) }
+      .groupByKey()
+      .map { m => m._2.mkString(" ") }
 
-        val members = centers.distanceJoin(p1, Array("x1", "y1"), Array("x","y"), (epsilon/2) + 0.01)
-          .select("x1", "y1", "id")
-          .rdd
-          .map{ d => (d(0) + "-" + d(1), d(2)) }
-          .groupByKey()
-          .map{ m => m._2.mkString(" ") }
+    val n = members.count()
 
-        val n = members.count()
-        println(n)
+    val ts = new ArrayList[ArrayList[Integer]]()
+    members.collect().foreach { x =>
+      val arrList = new ArrayList[Integer]()
+      x.split(" ").map(y => arrList.add(y.toInt))
+      Collections.sort(arrList)
+      ts.add(arrList)
+    }
 
-        val ts = new util.ArrayList[util.ArrayList[Integer]]()
-        members.collect().foreach{ x =>
-          val arrList = new util.ArrayList[Integer]()
-          x.split(" ").map( y => arrList.add(y.toInt) )
-          Collections.sort(arrList)
-          ts.add(arrList)
-        }
+    val minsup = 1
+    time1 = System.currentTimeMillis()
+    val dataset = new Dataset(ts)
+    val lcm = new AlgoLCM
+    var itemsets = lcm.runAlgorithm(minsup, dataset)
+    //lcm.printStats
+    //itemsets.printItemsets
+    time2 = System.currentTimeMillis()
+    val lcmTime = (time2 - time1) / 1000.0
+    val lcmNItemsets = itemsets.countItemsets(3)
 
-        val minsup = 1
-        val dataset = new Dataset(ts)
-        val lcm = new AlgoLCM
-        var itemsets = lcm.runAlgorithm(minsup, dataset)
-        lcm.printStats
-        //itemsets.printItemsets
-        println(itemsets.countItemsets(3))
+    time1 = System.currentTimeMillis()
+    val fpmax = new AlgoFPMax
+    itemsets = fpmax.runAlgorithm(ts, 1)
+    //fpmax.printStats
+    //itemsets.printItemsets
+    time2 = System.currentTimeMillis()
+    val fpMaxTime = (time2 - time1) / 1000.0
+    val fpMaxNItemsets = itemsets.countItemsets(3)
 
-        val fpmax = new AlgoFPMax
-        itemsets = fpmax.runAlgorithm(ts, 1)
-        fpmax.printStats
-        //itemsets.printItemsets
-        println(itemsets.countItemsets(3))
+    println("PBFE3,"
+      + epsilon + ","
+      + tag + ","
+      + 2 * ndisks + ","
+      + diskGenerationTime + ","
+      + lcmTime + ","
+      + fpMaxTime + ","
+      + lcmNItemsets + ","
+      + fpMaxNItemsets + ","
+      + Calendar.getInstance().getTime)
 
     sc.stop()
   }
--- Y2Q2/Research/Code/runExperimentsBeijing.sh
+++ Y2Q2/Research/Code/runExperimentsBeijing.sh
@@ -1,30 +1,14 @@
 #!/bin/bash
 
-START=10
-STEP=10
-<<<<<<< HEAD
-END=20
-
-for dataset in 10K #20K 30K 40K 50K #60K 70K 80K 90K 100K  
-do
-	for epsilon in `seq $START $STEP $END`
-	do
-		spark-submit --master local[*] target/scala-2.10/pbfe3_2.10-1.0.jar /opt/Datasets/Beijing/P${dataset}.csv $epsilon
-=======
-<<<<<<< HEAD
-END=50
-=======
-END=20
->>>>>>> parent of fc22eef... Still working in PBFE-SPMF implementation...
+START=2
+STEP=2
+END=10
 
 for dataset in 10K 20K 30K 40K 50K #60K 70K 80K 90K 100K  
 do
 	for epsilon in `seq $START $STEP $END`
 	do
-		spark-submit --packages com.databricks:spark-csv_2.10:1.5.0 --master local[*] pbfe.jar /opt/Datasets/Beijing/P${dataset}.csv $epsilon
-		cat tdisks/part-* > datasets/Beijing_N${dataset}_E${epsilon}.centers
-		rm -fR tdisks/
->>>>>>> origin/master
+		java -jar PBFE3/target/build/pbfe3.jar /opt/Datasets/Beijing/P${dataset}.csv $epsilon
 	done
 done
 
