b7f0eeeeb7cc55846dc8b1fc64f4825c1d71c5af
Andres Calderon <andress.calderon@gmail.com>
2017-05-15 23:06:36 -0700

    Adding a draft for the CS236 Project...

---
+++ Y2Q3/Project/Report/final/images.pl
@@ -0,0 +1,42 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate images original text with physical files.
+
+
+$key = q/varepsilon;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="13" HEIGHT="19" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="$\varepsilon $">|; 
+
+$key = q/approx;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="19" HEIGHT="19" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img10.png"
+ ALT="$\approx$">|; 
+
+$key = q/{{{p_1,p_2{{},{{d(p_1,p_2)leqvarepsilon;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="165" HEIGHT="39" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$\{p_1,p_2\},\d(p_1,p_2) \leq \varepsilon $">|; 
+
+$key = q/{p_1,p_2},d(p_1,p_2)leqvarepsilon;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="181" HEIGHT="39" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img8.png"
+ ALT="$\{p_1,p_2\}, d(p_1,p_2) \leq \varepsilon$">|; 
+
+$key = q/mathcal{O}(2n^2);MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="63" HEIGHT="39" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="$\mathcal{O}(2n^2)$">|; 
+
+$key = q/frac{varepsilon}{2};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="15" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img7.png"
+ ALT="$\frac{\varepsilon}{2}$">|; 
+
+1;
+
