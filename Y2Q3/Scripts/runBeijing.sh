d102f898178599be41aeb979b68a62fe440faadc
aocalderon <andress.calderon@gmail.com>
2017-06-02 17:28:48 -0700

    Sync from dblab-rack11...

--- Y2Q3/Scripts/runBeijing.sh
+++ Y2Q3/Scripts/runBeijing.sh
@@ -2,7 +2,7 @@
 
 spark-submit ~/PhD/Y2Q3/PFlock/target/scala-2.11/pflock_2.11-1.0.jar \
 	--prefix /home/acald013/Datasets/Beijing/P \
-	--estart 10 --eend 50 --estep 10 \
-	--dstart 10 --dend 50 --dstep 10 \
-	--partitions 32 \
+	--estart 50 --eend 100 --estep 10 \
+	--dstart 50 --dend 100 --dstep 10 \
+	--partitions 64 \
 	--output Beijing 
