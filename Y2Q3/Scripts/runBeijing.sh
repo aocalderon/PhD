b6ad5eb47724e6c3b5c5b595c66275def65c3bf2
aocalderon <andress.calderon@gmail.com>
2017-06-06 20:08:20 -0700

    Adding plot support between servers...

--- Y2Q3/Scripts/runBeijing.sh
+++ Y2Q3/Scripts/runBeijing.sh
@@ -2,17 +2,18 @@
 
 TS=`date +%s`
 DSTART=10
-DEND=1
+DEND=20
 SUFFIX="K"
 ESTART=10.0
-EEND=10.0
+EEND=20.0
 OUTPUT="Beijing"
 spark-submit ~/PhD/Y2Q3/PFlock/target/scala-2.11/pflock_2.11-1.0.jar \
 	--prefix /home/acald013/Datasets/Beijing/P \
 	--estart $ESTART --eend $EEND --estep 2 \
 	--dstart $DSTART --dend $DEND --dstep 10 \
 	--partitions 16 \
+	--tag $TS \
 	--output $OUTPUT
-FILENAME="${OUTPUT}_N${DSTART}${SUFIX}-${DEND}${SUFFIX}_E${ESTART}-${EEND}_${TS}.csv"
+FILENAME="${OUTPUT}_N${DSTART}${SUFFIX}-${DEND}${SUFFIX}_E${ESTART}-${EEND}_${TS}.csv"
 scp -i ~/.ssh/id_rsa $FILENAME acald013@bolt.cs.ucr.edu:/home/csgrads/acald013/public_html/public/Results 
 ssh -i ~/.ssh/id_rsa -t acald013@bolt.cs.ucr.edu "plotBenchmarks $FILENAME"
