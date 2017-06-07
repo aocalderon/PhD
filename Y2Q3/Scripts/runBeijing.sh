bf38c044826b3c706bff48ee8d032e1f9c748704
aocalderon <andress.calderon@gmail.com>
2017-06-07 00:28:47 -0700

    Adding plots...

--- Y2Q3/Scripts/runBeijing.sh
+++ Y2Q3/Scripts/runBeijing.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 TS=`date +%s`
-DSTART=20
-DEND=50
+PARTITIONS=32
+DSTART=60
+DEND=100
 SUFFIX="K"
-ESTART=10.0
-EEND=50.0
+ESTART=5.0
+EEND=30.0
 OUTPUT="Beijing"
 spark-submit ~/PhD/Y2Q3/PFlock/target/scala-2.11/pflock_2.11-1.0.jar \
 	--prefix /home/acald013/Datasets/Beijing/P \
 	--estart $ESTART --eend $EEND --estep 5 \
 	--dstart $DSTART --dend $DEND --dstep 10 \
-	--partitions 16 \
+	--partitions $PARTITIONS \
 	--tag $TS \
 	--output $OUTPUT
 FILENAME="${OUTPUT}_N${DSTART}${SUFFIX}-${DEND}${SUFFIX}_E${ESTART}-${EEND}_${TS}.csv"
@@ -20,4 +21,5 @@ ssh -i ~/.ssh/id_rsa -t acald013@bolt.cs.ucr.edu "plotBenchmarks $FILENAME"
 cd ~/PhD/
 git add --all
 git commit -m "Adding plots..."
+git pull
 git push
