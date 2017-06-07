031a9816a88c0097ed932471f30958d498cf29e0
aocalderon <andress.calderon@gmail.com>
2017-06-06 22:51:22 -0700

    Adding plots...

--- Y2Q3/Scripts/runBeijing.sh
+++ Y2Q3/Scripts/runBeijing.sh
@@ -17,3 +17,7 @@ spark-submit ~/PhD/Y2Q3/PFlock/target/scala-2.11/pflock_2.11-1.0.jar \
 FILENAME="${OUTPUT}_N${DSTART}${SUFFIX}-${DEND}${SUFFIX}_E${ESTART}-${EEND}_${TS}.csv"
 scp -i ~/.ssh/id_rsa $FILENAME acald013@bolt.cs.ucr.edu:/home/csgrads/acald013/public_html/public/Results 
 ssh -i ~/.ssh/id_rsa -t acald013@bolt.cs.ucr.edu "plotBenchmarks $FILENAME"
+cd ~/PhD/
+git add --all
+git commit -m "Adding plots..."
+git push
