b1900273c945ca4eef2d06e17ec18165d11af7cb
aocalderon <andress.calderon@gmail.com>
2017-06-06 22:04:30 -0700

    Running a new test...

--- Y2Q3/Scripts/runBeijing.sh
+++ Y2Q3/Scripts/runBeijing.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 TS=`date +%s`
-DSTART=10
-DEND=20
+DSTART=20
+DEND=50
 SUFFIX="K"
 ESTART=10.0
-EEND=20.0
+EEND=50.0
 OUTPUT="Beijing"
 spark-submit ~/PhD/Y2Q3/PFlock/target/scala-2.11/pflock_2.11-1.0.jar \
 	--prefix /home/acald013/Datasets/Beijing/P \
-	--estart $ESTART --eend $EEND --estep 2 \
+	--estart $ESTART --eend $EEND --estep 5 \
 	--dstart $DSTART --dend $DEND --dstep 10 \
 	--partitions 16 \
 	--tag $TS \
