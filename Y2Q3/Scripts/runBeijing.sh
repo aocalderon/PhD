2943eabf25386f5dcb496f13bef05b976526cab4
aocalderon <andress.calderon@gmail.com>
2017-06-10 22:14:21 -0700

    Adding plots...

--- Y2Q3/Scripts/runBeijing.sh
+++ Y2Q3/Scripts/runBeijing.sh
@@ -18,6 +18,9 @@ spark-submit ~/PhD/Y2Q3/PFlock/target/scala-2.11/pflock_2.11-1.0.jar \
 --estart $ESTART \
 --eend $EEND \
 --estep 5 \
+--dstart $DSTART \
+--dend $DEND \
+--dstep 10 \
 --output $OUTPUT
 #spark-submit ~/PhD/Y2Q3/PFlock/target/scala-2.11/pflock_2.11-1.0.jar \
 #	--prefix /home/acald013/Datasets/Beijing/P \
