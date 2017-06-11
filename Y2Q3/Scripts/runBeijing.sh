e09320187961dbca63a01e5cac379c06f3448799
aocalderon <andress.calderon@gmail.com>
2017-06-10 22:07:50 -0700

    Adding plots...

--- Y2Q3/Scripts/runBeijing.sh
+++ Y2Q3/Scripts/runBeijing.sh
@@ -13,7 +13,8 @@ echo "Running in $CORES cores..."
 spark-submit ~/PhD/Y2Q3/PFlock/target/scala-2.11/pflock_2.11-1.0.jar \
 --prefix /home/acald013/Datasets/Beijing/P \
 --master spark://169.235.27.134:7077 \
---cores $CORES --output $OUTPUT
+--cores $CORES \
+--output $OUTPUT
 #spark-submit ~/PhD/Y2Q3/PFlock/target/scala-2.11/pflock_2.11-1.0.jar \
 #	--prefix /home/acald013/Datasets/Beijing/P \
 #	--cores 3 \
