d8fe35a6acd6fbe79c220c0af822389326249f96
aocalderon <andress.calderon@gmail.com>
2017-06-10 22:31:23 -0700

    Adding plots...

--- Y2Q3/Scripts/runBeijing.sh
+++ Y2Q3/Scripts/runBeijing.sh
@@ -2,17 +2,18 @@
 
 CORES=$1
 TS=`date +%s`
-PARTITIONS=10
-DSTART=50
-DEND=50
+PARTITIONS=8
+DSTART=10
+DEND=30
 SUFFIX="K"
-ESTART=50.0
-EEND=50.0
+ESTART=5.0
+EEND=45.0
 OUTPUT="Beijing"
 echo "Running in $CORES cores..."
 spark-submit ~/PhD/Y2Q3/PFlock/target/scala-2.11/pflock_2.11-1.0.jar \
 --prefix /home/acald013/Datasets/Beijing/P \
 --master spark://169.235.27.134:7077 \
+#--master local[*]\
 --cores $CORES \
 --partitions $PARTITIONS \
 --tag $TS \
@@ -23,16 +24,6 @@ spark-submit ~/PhD/Y2Q3/PFlock/target/scala-2.11/pflock_2.11-1.0.jar \
 --dend $DEND \
 --dstep 10 \
 --output $OUTPUT
-#spark-submit ~/PhD/Y2Q3/PFlock/target/scala-2.11/pflock_2.11-1.0.jar \
-#	--prefix /home/acald013/Datasets/Beijing/P \
-#	--cores 3 \
-#	--estart $ESTART --eend $EEND --estep 5 \
-#	--dstart $DSTART --dend $DEND --dstep 10 \
-#	--partitions $PARTITIONS \
-#	--tag $TS \
-	#--master local[*]\
-#	--master spark://169.235.27.134:7077 \
-#	--output $OUTPUT
 TS2=`date +%s`
 DELAY=printf %.2f $(echo "($TS2-$TS1)/60" | bc -l)
 echo "Done at ... ${DELAY}s"
