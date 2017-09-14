af1228623319c09db6fab86b5aaef33fce6f866a
Andres Calderon <acald013@ucr.edu>
2017-09-13 18:05:32 -0700

    Exploring different values for mu...

--- Y2Q4/runJob.sh
+++ Y2Q4/runJob.sh
@@ -1,4 +1,4 @@
-Scripts/runBerlin.sh 28 2048
-Scripts/runBerlin.sh 28 1024
-Scripts/runBerlin.sh 28 512
-Scripts/runBerlin.sh 28 256
+Scripts/runBerlin.sh 28 1024 30
+Scripts/runBerlin.sh 28 1024 40
+Scripts/runBerlin.sh 28 1024 50
+
