883da80f74599439e55cbc2e32cc05983fdd07e3
Andres Calderon <acald013@ucr.edu>
2017-09-15 09:03:51 -0700

    Adding experiments results...

--- Y2Q4/runJob.sh
+++ Y2Q4/runJob.sh
@@ -1,4 +1,6 @@
-Scripts/runBerlin.sh 28 1024 30
-Scripts/runBerlin.sh 28 1024 40
-Scripts/runBerlin.sh 28 1024 50
-
+Scripts/runBerlin.sh 6 1024 50
+Scripts/runBerlin.sh 6 1024 50
+Scripts/runBerlin.sh 5 1024 50
+Scripts/runBerlin.sh 5 1024 50
+Scripts/runBerlin.sh 4 1024 50
+Scripts/runBerlin.sh 4 1024 50
