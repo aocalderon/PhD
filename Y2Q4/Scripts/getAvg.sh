df8f7b01e79fb0302a3ce73cebabfdf4b264a890
Andres Calderon <acald013@ucr.edu>
2017-09-16 10:34:03 -0700

    Updating files...

--- Y2Q4/Scripts/getAvg.sh
+++
@@ -1,1 +0,0 @@
-/home/acald013/opt/src/R-3.3.3/bin/R -q -e "x <- read.csv('mbrs_40.0_1024.csv', header = F); summary(x$V2); sd(x[,2])"
--- Y2Q4/runJob.sh
+++
@@ -1,6 +0,0 @@
-Scripts/runBerlin.sh 6 1024 50
-Scripts/runBerlin.sh 6 1024 50
-Scripts/runBerlin.sh 5 1024 50
-Scripts/runBerlin.sh 5 1024 50
-Scripts/runBerlin.sh 4 1024 50
-Scripts/runBerlin.sh 4 1024 50
