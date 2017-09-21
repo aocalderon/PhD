cde008ad1171f76eddcffb0b73a5764ce3cd056e
Andres Calderon <acald013@ucr.edu>
2017-09-21 10:14:38 -0700

    Sycn...

--- Y2Q4/Scripts/Scaleup/runScaleup.sh
+++ Y2Q4/Scripts/Scaleup/runScaleup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 N=3
-MU=20
+MU=50
+PARTITIONS=1024
 
 # Running Scaleup on 1 Node with 20K dataset...
 $SPARK_HOME/sbin/stop-all.sh
@@ -10,7 +11,7 @@ echo "acald013@dblab-rack11" >> $SPARK_HOME/conf/slaves
 $SPARK_HOME/sbin/start-all.sh
 for i in `seq 1 $N`
 do
-	./runDataset.sh 20 7 1024 $MU
+	./runDataset.sh 20 7 $PARTITIONS $MU
 done
 
 # Running Scaleup on 2 Nodes with 40K dataset...
@@ -22,7 +23,7 @@ $SPARK_HOME/sbin/start-all.sh
 
 for i in `seq 1 $N`
 do
-        ./runDataset.sh 40 14 1024 $MU
+        ./runDataset.sh 40 14 $PARTITIONS $MU
 done
 
 # Running Scaleup on 3 Nodes with 60K dataset...
@@ -35,7 +36,7 @@ $SPARK_HOME/sbin/start-all.sh
 
 for i in `seq 1 $N`
 do
-        ./runDataset.sh 60 21 1024 $MU
+        ./runDataset.sh 60 21 $PARTITIONS $MU
 done
 
 # Running Scaleup on 4 Nodes with 80K dataset...
@@ -49,7 +50,7 @@ $SPARK_HOME/sbin/start-all.sh
 
 for i in `seq 1 $N`
 do
-        ./runDataset.sh 80 28 1024 $MU
+        ./runDataset.sh 80 28 $PARTITIONS $MU
 done
 
 $SPARK_HOME/sbin/stop-all.sh
