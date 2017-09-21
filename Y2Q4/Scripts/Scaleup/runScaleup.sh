1a4b006dfb44f51b3ded4f4c59dd8255200cdc68
Andres Calderon <acald013@ucr.edu>
2017-09-20 18:19:19 -0700

    Updating speedup on single node experiments...

--- Y2Q4/Scripts/Scaleup/runScaleup.sh
+++ Y2Q4/Scripts/Scaleup/runScaleup.sh
@@ -5,7 +5,7 @@ N=3
 # Running Scaleup on 1 Node with 20K dataset...
 $SPARK_HOME/sbin/stop-all.sh
 truncate -s 0 $SPARK_HOME/conf/slaves
-echo "acald013@dblab-rack15" >> $SPARK_HOME/conf/slaves
+echo "acald013@dblab-rack11" >> $SPARK_HOME/conf/slaves
 $SPARK_HOME/sbin/start-all.sh
 for i in `seq 1 $N`
 do
@@ -15,8 +15,8 @@ done
 # Running Scaleup on 2 Nodes with 40K dataset...
 $SPARK_HOME/sbin/stop-all.sh
 truncate -s 0 $SPARK_HOME/conf/slaves
-echo "acald013@dblab-rack15" >> $SPARK_HOME/conf/slaves
-echo "acald013@dblab-rack14" >> $SPARK_HOME/conf/slaves
+echo "acald013@dblab-rack11" >> $SPARK_HOME/conf/slaves
+echo "acald013@dblab-rack12" >> $SPARK_HOME/conf/slaves
 $SPARK_HOME/sbin/start-all.sh
 
 for i in `seq 1 $N`
@@ -27,9 +27,9 @@ done
 # Running Scaleup on 3 Nodes with 60K dataset...
 $SPARK_HOME/sbin/stop-all.sh
 truncate -s 0 $SPARK_HOME/conf/slaves
-echo "acald013@dblab-rack15" >> $SPARK_HOME/conf/slaves
-echo "acald013@dblab-rack14" >> $SPARK_HOME/conf/slaves
+echo "acald013@dblab-rack11" >> $SPARK_HOME/conf/slaves
 echo "acald013@dblab-rack12" >> $SPARK_HOME/conf/slaves
+echo "acald013@dblab-rack14" >> $SPARK_HOME/conf/slaves
 $SPARK_HOME/sbin/start-all.sh
 
 for i in `seq 1 $N`
@@ -40,10 +40,10 @@ done
 # Running Scaleup on 4 Nodes with 80K dataset...
 $SPARK_HOME/sbin/stop-all.sh
 truncate -s 0 $SPARK_HOME/conf/slaves
-echo "acald013@dblab-rack15" >> $SPARK_HOME/conf/slaves
-echo "acald013@dblab-rack14" >> $SPARK_HOME/conf/slaves
-echo "acald013@dblab-rack12" >> $SPARK_HOME/conf/slaves
 echo "acald013@dblab-rack11" >> $SPARK_HOME/conf/slaves
+echo "acald013@dblab-rack12" >> $SPARK_HOME/conf/slaves
+echo "acald013@dblab-rack14" >> $SPARK_HOME/conf/slaves
+echo "acald013@dblab-rack15" >> $SPARK_HOME/conf/slaves
 $SPARK_HOME/sbin/start-all.sh
 
 for i in `seq 1 $N`
