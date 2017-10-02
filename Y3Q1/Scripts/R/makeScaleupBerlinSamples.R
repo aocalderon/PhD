001751180fae4e237a7dda4d6dfacbdb7522cdbe
aocalderon <andress.calderon@gmail.com>
2017-10-02 11:30:14 -0700

    Fixing a minor issue...

--- Y3Q1/Scripts/R/makeScaleupBerlinSamples.R
+++ Y3Q1/Scripts/R/makeScaleupBerlinSamples.R
@@ -13,4 +13,4 @@ B40K = B80K[sort(c(i1,i2)),]
 B60K = B80K[sort(c(i1,i2,i3)),]
 write.table(B20K,paste0(PHD_HOME,PATH,'B20K.csv'),row.names = F, col.names = F, sep = ',', quote = F)
 write.table(B40K,paste0(PHD_HOME,PATH,'B40K.csv'),row.names = F, col.names = F, sep = ',', quote = F)
-write.table(B60K,paste0(PHD_HOME,PATH,'B50K.csv'),row.names = F, col.names = F, sep = ',', quote = F)
+write.table(B60K,paste0(PHD_HOME,PATH,'B60K.csv'),row.names = F, col.names = F, sep = ',', quote = F)
