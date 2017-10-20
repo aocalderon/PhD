44a784be8d4d887d75769cecaeba9531e2128409
Andres Calderon <acald013@ucr.edu>
2017-10-20 09:16:39 -0700

    Adding optimizer results...

--- Y3Q1/Scripts/Misc/Optimizer.sh
+++ Y3Q1/Scripts/Misc/Optimizer.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
-for dataset in B40K B60K B80K
+EPSILON=50
+MU=12
+#MASTER="spark://169.235.27.138:7077"
+MASTER="local[*]"
+CORES=28
+
+$SPARK_HOME/sbin/stop-all.sh
+truncate -s 0 $SPARK_HOME/conf/slaves
+echo "acald013@dblab-rack11" >> $SPARK_HOME/conf/slaves
+echo "acald013@dblab-rack12" >> $SPARK_HOME/conf/slaves
+echo "acald013@dblab-rack14" >> $SPARK_HOME/conf/slaves
+echo "acald013@dblab-rack15" >> $SPARK_HOME/conf/slaves
+$SPARK_HOME/sbin/start-all.sh
+`rm /tmp/Andres_Spark*.log`
+
+for dataset in B40K
 do
-	for entries in `seq 2 6 26`
+	for entries in `seq 14 6 26`
 	do
-		for partitions in `seq 512 512 3072`
+		for partitions in `seq 2048 512 3072`
 		do
-			spark-submit --class Test /home/acald013/PhD/Y3Q1/PFlock/target/scala-2.11/pflock_2.11-2.0.jar $dataset $entries $partitions spark://169.235.27.138:7077 28
+			spark-submit --class Optimizer /home/acald013/PhD/Y3Q1/PFlock/target/scala-2.11/pflock_2.11-2.0.jar \
+				$dataset \
+				$entries \
+				$partitions \
+				$EPSILON \
+				$MU \
+				$MASTER \
+				$CORES
 		done
 	done
 done
-# spark-submit --master spark://169.235.27.138:7077 pbfe3.jar ~/Datasets/Beijing/P${dataset}.csv $epsilon $1
