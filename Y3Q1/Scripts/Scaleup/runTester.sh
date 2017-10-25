710696a13cca1626dbdbbf52793b02424ce89e69
Andres Calderon <acald013@ucr.edu>
2017-10-25 09:31:08 -0700

    Collecting candidate disks files for testing...

--- Y3Q1/Scripts/Scaleup/runTester.sh
+++ Y3Q1/Scripts/Scaleup/runTester.sh
@@ -1,31 +1,6 @@
 #!/bin/bash
 
-spark-submit --class PartitionViewer /home/acald013/PhD/Y3Q1/PFlock/target/scala-2.11/pflock_2.11-2.0.jar \
-B60K 25 64 40 12 spark://169.235.27.138:7077 28
+spark-submit --class PartitionSaver /home/acald013/PhD/Y3Q1/PFlock/target/scala-2.11/pflock_2.11-2.0.jar B60K 25 1024 40 12 spark://169.235.27.138:7077 28
 
-spark-submit --class PartitionViewer /home/acald013/PhD/Y3Q1/PFlock/target/scala-2.11/pflock_2.11-2.0.jar \
-B60K 25 128 40 12 spark://169.235.27.138:7077 28
+spark-submit --class PartitionSaver /home/acald013/PhD/Y3Q1/PFlock/target/scala-2.11/pflock_2.11-2.0.jar B60K 25 1024 50 12 spark://169.235.27.138:7077 28
 
-spark-submit --class PartitionViewer /home/acald013/PhD/Y3Q1/PFlock/target/scala-2.11/pflock_2.11-2.0.jar \
-B60K 25 256 40 12 spark://169.235.27.138:7077 28
-
-spark-submit --class PartitionViewer /home/acald013/PhD/Y3Q1/PFlock/target/scala-2.11/pflock_2.11-2.0.jar \
-B60K 25 512 40 12 spark://169.235.27.138:7077 28
-
-spark-submit --class PartitionViewer /home/acald013/PhD/Y3Q1/PFlock/target/scala-2.11/pflock_2.11-2.0.jar \
-B60K 25 1024 40 12 spark://169.235.27.138:7077 28
-
-spark-submit --class PartitionViewer /home/acald013/PhD/Y3Q1/PFlock/target/scala-2.11/pflock_2.11-2.0.jar \
-B60K 25 64 50 12 spark://169.235.27.138:7077 28
-
-spark-submit --class PartitionViewer /home/acald013/PhD/Y3Q1/PFlock/target/scala-2.11/pflock_2.11-2.0.jar \
-B60K 25 128 50 12 spark://169.235.27.138:7077 28
-
-spark-submit --class PartitionViewer /home/acald013/PhD/Y3Q1/PFlock/target/scala-2.11/pflock_2.11-2.0.jar \
-B60K 25 256 50 12 spark://169.235.27.138:7077 28
-
-spark-submit --class PartitionViewer /home/acald013/PhD/Y3Q1/PFlock/target/scala-2.11/pflock_2.11-2.0.jar \
-B60K 25 512 50 12 spark://169.235.27.138:7077 28
-
-spark-submit --class PartitionViewer /home/acald013/PhD/Y3Q1/PFlock/target/scala-2.11/pflock_2.11-2.0.jar \
-B60K 25 1024 50 12 spark://169.235.27.138:7077 28
