3f884d828615a7c8b4f8defea757575cd0d5c6e7
Andres Calderon <andress.calderon@gmail.com>
2017-11-16 21:23:56 -0800

    Working on a LCM tester...

--- Y3Q1/PFlock/src/main/scala/SPMF/UnoLCMRunner.scala
+++ Y3Q1/PFlock/src/main/scala/SPMF/UnoLCMRunner.scala
@@ -1,6 +1,9 @@
 package SPMF
 
+import java.io.PrintWriter
 import org.slf4j.{Logger, LoggerFactory}
+import scala.collection.mutable
+import scala.io.Source
 import scala.language.postfixOps
 import sys.process._
 
@@ -8,8 +11,25 @@ object UnoLCMRunner {
     private val logger: Logger = LoggerFactory.getLogger("myLogger")
 
     def main(args: Array[String]): Unit = {
-        logger.info("Running LCM...")
-        "lcm M /home/acald013/PhD/Y3Q1/PFlock/src/main/scala/SPMF/input2.txt 1 -" !
+        var data = new mutable.HashSet[(Int, Set[Int])]
+        val lines = Source.fromFile("/home/and/tmp/DB80K_E100.0_M50_Maximals.txt")
+        for(line <- lines.getLines()){
+            val IdAndTransaction = line.split(";")
+            val tid = IdAndTransaction(0).toInt
+            val transaction = IdAndTransaction(1).split(" ").map(_.toInt).toList.sorted.toSet
+            data += (tid -> transaction)
+        }
+        lines.close()
+        val transactionsByTID = data.groupBy(x => x._1).mapValues(v => v.map(m => m._2))
+        for(tid <- transactionsByTID.keys){
+            val output = transactionsByTID.get(tid).map(t => t.mkString(" ")).toList.mkString("\n")
+            new PrintWriter("/tmp/input%d.txt".format(tid)){
+                write(output)
+                close()
+            }
+            logger.info("lcm M /tmp/input%d.txt 1 -".format(tid))
+            "lcm M /tmp/input%d.txt 1 -".format(tid) !
+        }
     }
 }
 
