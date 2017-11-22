0d5c695e566c08a7104b4e51c14a0f5b2ccf853e
Andres Calderon <acald013@ucr.edu>
2017-11-22 11:11:37 -0800

    Running additional set of experiments...

--- Y3Q1/Scripts/Scaleup/runScaleup.sh
+++ Y3Q1/Scripts/Scaleup/runScaleup.sh
@@ -6,14 +6,14 @@ CANDIDATES=128
 # Running Scaleup on 1 Node with 20K dataset...
 $SPARK_HOME/sbin/stop-all.sh
 truncate -s 0 $SPARK_HOME/conf/slaves
-echo "acald013@dblab-rack11" >> $SPARK_HOME/conf/slaves
+echo "acald013@dblab-rack12" >> $SPARK_HOME/conf/slaves
 $SPARK_HOME/sbin/start-all.sh
 for i in `seq 1 $N`
 do
 	echo "Running iteration $i/$N ..."
-	./runDataset.sh B20K $CANDIDATES 10 5 7
-	./runDataset.sh B20K $CANDIDATES 20 10 7
-	./runDataset.sh B20K $CANDIDATES 30 15 7
+	#./runDataset.sh B20K $CANDIDATES 10 5 7
+	#./runDataset.sh B20K $CANDIDATES 20 10 7
+	#./runDataset.sh B20K $CANDIDATES 30 15 7
 	./runDataset.sh B20K $CANDIDATES 40 30 7
 	./runDataset.sh B20K $CANDIDATES 50 40 7
 done
@@ -21,16 +21,16 @@ done
 # Running Scaleup on 2 Nodes with 40K dataset...
 $SPARK_HOME/sbin/stop-all.sh
 truncate -s 0 $SPARK_HOME/conf/slaves
-echo "acald013@dblab-rack11" >> $SPARK_HOME/conf/slaves
+echo "acald013@dblab-rack14" >> $SPARK_HOME/conf/slaves
 echo "acald013@dblab-rack12" >> $SPARK_HOME/conf/slaves
 $SPARK_HOME/sbin/start-all.sh
 
 for i in `seq 1 $N`
 do
 	echo "Running iteration $i/$N ..."
-	./runDataset.sh B40K $CANDIDATES 10 5 14
-	./runDataset.sh B40K $CANDIDATES 20 10 14
-	./runDataset.sh B40K $CANDIDATES 30 15 14
+	#./runDataset.sh B40K $CANDIDATES 10 5 14
+	#./runDataset.sh B40K $CANDIDATES 20 10 14
+	#./runDataset.sh B40K $CANDIDATES 30 15 14
 	./runDataset.sh B40K $CANDIDATES 40 30 14
 	./runDataset.sh B40K $CANDIDATES 50 40 14
 done
@@ -46,9 +46,9 @@ $SPARK_HOME/sbin/start-all.sh
 for i in `seq 1 $N`
 do
 	echo "Running iteration $i/$N ..."
-	./runDataset.sh B60K $CANDIDATES 10 5 21
-	./runDataset.sh B60K $CANDIDATES 20 10 21
-	./runDataset.sh B60K $CANDIDATES 30 15 21
+	#./runDataset.sh B60K $CANDIDATES 10 5 21
+	#./runDataset.sh B60K $CANDIDATES 20 10 21
+	#./runDataset.sh B60K $CANDIDATES 30 15 21
 	./runDataset.sh B60K $CANDIDATES 40 30 21
 	./runDataset.sh B60K $CANDIDATES 50 40 21
 done
@@ -65,9 +65,9 @@ $SPARK_HOME/sbin/start-all.sh
 for i in `seq 1 $N`
 do
 	echo "Running iteration $i/$N ..."
-	./runDataset.sh B80K $CANDIDATES 10 5 28
-	./runDataset.sh B80K $CANDIDATES 20 10 28
-	./runDataset.sh B80K $CANDIDATES 30 15 28
+	#./runDataset.sh B80K $CANDIDATES 10 5 28
+	#./runDataset.sh B80K $CANDIDATES 20 10 28
+	#./runDataset.sh B80K $CANDIDATES 30 15 28
 	./runDataset.sh B80K $CANDIDATES 40 30 28
 	./runDataset.sh B80K $CANDIDATES 50 40 28
 done
