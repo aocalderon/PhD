6b0baf63fe7c8b002a8f173338a7faf37d60dbc8
Andres Calderon <acald013@ucr.edu>
2017-11-28 05:07:57 -0800

    Adding a new set of experiments with the new datasets...

--- Y3Q1/Scripts/Scaleup/runDataset.sh
+++ Y3Q1/Scripts/Scaleup/runDataset.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 DATASET=$1
-CANDIDATES=$2
-EPSILON=$3
-MU=$4
-CORES=$5
+EPSILON=$2
+MU=$3
+CORES=$4
 
 spark-submit --class MaximalFinderExpansion /home/acald013/PhD/Y3Q1/PFlock/target/scala-2.11/pflock_2.11-2.0.jar \
 --dataset $DATASET \
--- Y3Q1/Scripts/Scaleup/runScaleup.sh
+++ Y3Q1/Scripts/Scaleup/runScaleup.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 N=5
-CANDIDATES=128
 
 # Running Scaleup on 1 Node with 20K dataset...
 $SPARK_HOME/sbin/stop-all.sh
 truncate -s 0 $SPARK_HOME/conf/slaves
 echo "acald013@dblab-rack12" >> $SPARK_HOME/conf/slaves
 $SPARK_HOME/sbin/start-all.sh
+
+DATASET="B20K"
+CORES=7
 for i in `seq 1 $N`
 do
-	echo "Running iteration $i/$N ..."
-	#./runDataset.sh B20K $CANDIDATES 10 5 7
-	#./runDataset.sh B20K $CANDIDATES 20 10 7
-	#./runDataset.sh B20K $CANDIDATES 30 15 7
-	#./runDataset.sh B20K $CANDIDATES 40 30 7
-	#./runDataset.sh B20K $CANDIDATES 50 40 7
+	echo "Running iteration $i/$N for $DATASET ..."
+	./runDataset.sh $DATASET 10 5 $CORES
+	./runDataset.sh $DATASET 20 10 $CORES
+	./runDataset.sh $DATASET 30 15 $CORES
+	./runDataset.sh $DATASET 40 30 $CORES
+	./runDataset.sh $DATASET 50 40 $CORES
 done
 
 # Running Scaleup on 2 Nodes with 40K dataset...
@@ -25,14 +27,15 @@ echo "acald013@dblab-rack14" >> $SPARK_HOME/conf/slaves
 echo "acald013@dblab-rack12" >> $SPARK_HOME/conf/slaves
 $SPARK_HOME/sbin/start-all.sh
 
+DATASET="B40K"
 for i in `seq 1 $N`
 do
-	echo "Running iteration $i/$N ..."
-	#./runDataset.sh B40K $CANDIDATES 10 5 14
-	#./runDataset.sh B40K $CANDIDATES 20 10 14
-	#./runDataset.sh B40K $CANDIDATES 30 15 14
-	#./runDataset.sh B40K $CANDIDATES 40 30 14
-	#./runDataset.sh B40K $CANDIDATES 50 40 14
+	echo "Running iteration $i/$N for $DATASET ..."
+	./runDataset.sh B40K 10 5 14
+	./runDataset.sh B40K 20 10 14
+	./runDataset.sh B40K 30 15 14
+	./runDataset.sh B40K 40 30 14
+	./runDataset.sh B40K 50 40 14
 done
 
 # Running Scaleup on 3 Nodes with 60K dataset...
@@ -43,14 +46,15 @@ echo "acald013@dblab-rack12" >> $SPARK_HOME/conf/slaves
 echo "acald013@dblab-rack14" >> $SPARK_HOME/conf/slaves
 $SPARK_HOME/sbin/start-all.sh
 
+DATASET="B60K"
 for i in `seq 1 $N`
 do
-	echo "Running iteration $i/$N ..."
-	#./runDataset.sh B60K $CANDIDATES 10 5 21
-	#./runDataset.sh B60K $CANDIDATES 20 10 21
-	#./runDataset.sh B60K $CANDIDATES 30 15 21
-	./runDataset.sh B60K $CANDIDATES 40 30 21
-	./runDataset.sh B60K $CANDIDATES 50 40 21
+	echo "Running iteration $i/$N for $DATASET ..."
+	./runDataset.sh B60K 10 5 21
+	./runDataset.sh B60K 20 10 21
+	./runDataset.sh B60K 30 15 21
+	./runDataset.sh B60K 40 30 21
+	./runDataset.sh B60K 50 40 21
 done
 
 # Running Scaleup on 4 Nodes with 80K dataset...
@@ -62,14 +66,15 @@ echo "acald013@dblab-rack14" >> $SPARK_HOME/conf/slaves
 echo "acald013@dblab-rack15" >> $SPARK_HOME/conf/slaves
 $SPARK_HOME/sbin/start-all.sh
 
+DATASET="B80K"
 for i in `seq 1 $N`
 do
-	echo "Running iteration $i/$N ..."
-	#./runDataset.sh B80K $CANDIDATES 10 5 28
-	#./runDataset.sh B80K $CANDIDATES 20 10 28
-	#./runDataset.sh B80K $CANDIDATES 30 15 28
-	./runDataset.sh B80K $CANDIDATES 40 30 28
-	./runDataset.sh B80K $CANDIDATES 50 40 28
+	echo "Running iteration $i/$N for $DATASET ..."
+	./runDataset.sh B80K 10 5 28
+	./runDataset.sh B80K 20 10 28
+	./runDataset.sh B80K 30 15 28
+	./runDataset.sh B80K 40 30 28
+	./runDataset.sh B80K 50 40 28
 done
 
 $SPARK_HOME/sbin/stop-all.sh
