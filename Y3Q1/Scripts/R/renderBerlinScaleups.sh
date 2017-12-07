77edc492438c2b0cf1a29c90f1eb1ef598370f17
Andres Calderon <andress.calderon@gmail.com>
2017-12-07 08:47:35 -0800

    Sync...

---
+++ Y3Q1/Scripts/R/renderBerlinScaleups.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+./renderPlotScaleupFromLogs2.R Y3Q1/Scripts/Scaleup/ Berlin_11-1112-111214-11121415 out
+./renderPlotScaleupFromLogs2.R Y3Q1/Scripts/Scaleup/ Berlin_11-1114-111214-11121415 out
+./renderPlotScaleupFromLogs2.R Y3Q1/Scripts/Scaleup/ Berlin_11-1214-111214-11121415 out
+./renderPlotScaleupFromLogs2.R Y3Q1/Scripts/Scaleup/ Berlin_12-1112-111214-11121415 out
+./renderPlotScaleupFromLogs2.R Y3Q1/Scripts/Scaleup/ Berlin_12-1114-111214-11121415 out
+./renderPlotScaleupFromLogs2.R Y3Q1/Scripts/Scaleup/ Berlin_12-1214-111214-11121415 out
+./renderPlotScaleupFromLogs2.R Y3Q1/Scripts/Scaleup/ Berlin_14-1112-111214-11121415 out
+./renderPlotScaleupFromLogs2.R Y3Q1/Scripts/Scaleup/ Berlin_14-1114-111214-11121415 out
+./renderPlotScaleupFromLogs2.R Y3Q1/Scripts/Scaleup/ Berlin_14-1214-111214-11121415 out
