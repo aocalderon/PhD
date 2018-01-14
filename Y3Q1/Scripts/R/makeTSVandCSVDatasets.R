206dd47471faf4ad79b969aa75e4fbe753246185
Andres Calderon <andress.calderon@gmail.com>
2018-01-14 15:54:53 -0800

    Making buses datasets...

--- Y3Q1/Scripts/R/makeTSVandCSVDatasets.R
+++ Y3Q1/Scripts/R/makeTSVandCSVDatasets.R
@@ -8,18 +8,18 @@ pacman::p_load(data.table, sqldf)
 
 PHD_HOME = Sys.getenv(c("PHD_HOME"))
 PATH = "Y3Q1/Datasets/Buses/"
+DATASET = "buses"
 EXTENSION = ".txt"
-DATASET = "Buses"
+SEPARATOR = "\t"
 filename = paste0(PHD_HOME,PATH,DATASET,EXTENSION)
-data = read.table(filename, header = F, sep = ',')
+data = read.table(filename, header = F, sep = SEPARATOR)
 
 ###################
 # Reading data...
 ###################
 
 data = as.data.table(data)
-names(data) = c('id', 'x', 'y')
-data$t = 0
+names(data) = c('id', 'x', 'y','t')
 
 ###################
 # Prunning possible duplicates...
@@ -31,7 +31,7 @@ data = data[ , list(id = min(id)), by = c('x', 'y', 't')]
 # Writing back...
 ###################
 
-write.table(data[ , c('id', 'x', 'y')]
+write.table(data[ , c('x', 'y','t','id')]
           , file = paste0(PHD_HOME,PATH,DATASET,"_PFlock.csv")
           , row.names = F
           , col.names = F
